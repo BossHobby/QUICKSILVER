0c5fb1feea00b3dd553e78bad217a3127045f315
NotFastEnuf <schrock7903@gmail.com>
2019-04-23 00:21:59 -0400

    set fmc to sector 3 with matchng address

--- Silverware/src/drv_fmc1.c
+++ Silverware/src/drv_fmc1.c
@@ -3,8 +3,27 @@
 #include "project.h"
 #include "drv_fmc.h"
 
+#ifdef F0
 // address 32768 - 1024 = 31k - last flash block
 #define FLASH_ADDR 0x08007C00
+#endif
+#ifdef F405
+/*
+Sector 0    0x08000000 - 0x08003FFF 16 Kbytes
+Sector 1    0x08004000 - 0x08007FFF 16 Kbytes
+Sector 2    0x08008000 - 0x0800BFFF 16 Kbytes
+Sector 3    0x0800C000 - 0x0800FFFF 16 Kbytes
+Sector 4    0x08010000 - 0x0801FFFF 64 Kbytes
+Sector 5    0x08020000 - 0x0803FFFF 128 Kbytes
+Sector 6    0x08040000 - 0x0805FFFF 128 Kbytes
+Sector 7    0x08060000 - 0x0807FFFF 128 Kbytes
+Sector 8    0x08080000 - 0x0809FFFF 128 Kbytes
+Sector 9    0x080A0000 - 0x080BFFFF 128 Kbytes
+Sector 10   0x080C0000 - 0x080DFFFF 128 Kbytes
+Sector 11   0x080E0000 - 0x080FFFFF 128 Kbytes
+*/
+#define FLASH_ADDR 0x0800C000  //sector 3 address
+#endif
 
 extern void failloop( int);
 
@@ -40,7 +59,7 @@ void fmc_lock() {
 int fmc_erase( void )
 {	
 	#ifdef F405
-	int test = FLASH_EraseSector( FLASH_Sector_1, VoltageRange_3);
+	int test = FLASH_EraseSector( FLASH_Sector_3, VoltageRange_3);
 	#endif	
 	
 	#ifdef F0
