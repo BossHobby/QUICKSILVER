96dd819983a399b3e7b7b86730aaa546c287fb60
bkleiner <b.kleiner@str8labs.com>
2019-07-25 00:42:04 +0200

    move f405 system, adjust projects files

--- QUICKSILVER/src/system/stm32f405/target.mk
+++ QUICKSILVER/src/system/stm32f405/target.mk
@@ -3,10 +3,11 @@ DEVICE_FLAGS = -DSTM32F4XX -DSTM32F40XX -DUSE_STDPERIPH_DRIVER
 
 TARGET_LD_SCRIPT = stm32f4_flash.ld
 
-TARGET_INCLUDE = src/system \
+TARGET_INCLUDE = src/system/stm32f405 \
 	Libraries/CMSIS/Include \
 	Libraries/Device/STM32F4xx/Include \
 	Libraries/STM32F4xx_StdPeriph_Driver/inc
 
 TARGET_SOURCE = $(wildcard Libraries/STM32F4xx_StdPeriph_Driver/src/*.c) \
-	src/system/startup_stm32f40xx.s
+	$(wildcard src/system/stm32f405/*.c) \
+	src/system/stm32f405/startup_stm32f40xx.s
