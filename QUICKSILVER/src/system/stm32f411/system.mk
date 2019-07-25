0358fb3df772ff0a116b52505f8b57759f139f87
bkleiner <b.kleiner@str8labs.com>
2019-07-25 10:41:17 +0200

    add per board config directories, create per board targets in cube

--- QUICKSILVER/src/system/stm32f411/system.mk
+++ QUICKSILVER/src/system/stm32f411/system.mk
@@ -1,13 +1,13 @@
 ARCH_FLAGS = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 
 DEVICE_FLAGS = -DSTM32F4XX -DSTM32F40XX -DUSE_STDPERIPH_DRIVER
 
-TARGET_LD_SCRIPT = stm32f4_flash.ld
+SYSTEM_LD_SCRIPT = stm32f4_flash.ld
 
-TARGET_INCLUDE = src/system/stm32f405 \
+SYSTEM_INCLUDE = src/system/stm32f405 \
 	Libraries/CMSIS/Include \
 	Libraries/Device/STM32F4xx/Include \
 	Libraries/STM32F4xx_StdPeriph_Driver/inc
 
-TARGET_SOURCE = $(wildcard Libraries/STM32F4xx_StdPeriph_Driver/src/*.c) \
+SYSTEM_SOURCE = $(wildcard Libraries/STM32F4xx_StdPeriph_Driver/src/*.c) \
 	$(wildcard src/system/stm32f405/*.c) \
 	src/system/stm32f405/startup_stm32f40xx.s
---
+++ QUICKSILVER/src/targets/target.mk
@@ -0,0 +1,18 @@
+# each directory represents a target
+TARGETS = $(sort $(notdir $(wildcard src/targets/*)))
+TARGET = $(foreach entry,$(MAKECMDGOALS),$(findstring $(entry),$(TARGETS)))
+
+ifeq ($(TARGET),)
+# default to build AWv2
+TARGET = alienwhoop_v2
+endif
+
+TARGET_DIR = src/targets/$(TARGET)
+include $(TARGET_DIR)/target.mk
+
+# we get SYSTEM from target
+SYSTEM_DIR = src/system/$(SYSTEM)
+include $(SYSTEM_DIR)/system.mk
+
+TARGET_INCLUDE = $(SYSTEM_INCLUDE) $(TARGET_DIR)
+TARGET_SOURCE = $(SYSTEM_SOURCE)
