376ead3b3b96aa50ac2c4d6abcfa30d7d4bd3697
bkleiner <b.kleiner@str8labs.com>
2019-08-02 04:15:26 +0200

    frsky: leave less resolution on the table

--- QUICKSILVER/src/main/rx/rx_frsky.c
+++ QUICKSILVER/src/main/rx/rx_frsky.c
@@ -46,7 +46,7 @@ static uint8_t list_length = FRSKY_HOPTABLE_SIZE;
 static uint8_t protocol_state = STATE_DETECT;
 static unsigned long time_tuned_ms;
 
-frsky_bind_data frsky_bind = {0xff, 0xff};
+frsky_bind_data frsky_bind = {{0xff, 0xff}};
 
 extern float rx[4];
 extern char aux[AUXNUMBER];
@@ -298,36 +298,29 @@ static uint8_t get_bind2(uint8_t *packet) {
 static void frsky_d_set_rc_data() {
   uint16_t channels[FRSKY_D_CHANNEL_COUNT];
 
-  channels[0] = (uint16_t)(((packet[10] & 0x0F) << 8 | packet[6]) * FRSKY_D_CHANNEL_SCALING);
-  channels[1] = (uint16_t)(((packet[10] & 0xF0) << 4 | packet[7]) * FRSKY_D_CHANNEL_SCALING);
-  channels[2] = (uint16_t)(((packet[11] & 0x0F) << 8 | packet[8]) * FRSKY_D_CHANNEL_SCALING);
-  channels[3] = (uint16_t)(((packet[11] & 0xF0) << 4 | packet[9]) * FRSKY_D_CHANNEL_SCALING);
-  channels[4] = (uint16_t)(((packet[16] & 0x0F) << 8 | packet[12]) * FRSKY_D_CHANNEL_SCALING);
-  channels[5] = (uint16_t)(((packet[16] & 0xF0) << 4 | packet[13]) * FRSKY_D_CHANNEL_SCALING);
-  channels[6] = (uint16_t)(((packet[17] & 0x0F) << 8 | packet[14]) * FRSKY_D_CHANNEL_SCALING);
-  channels[7] = (uint16_t)(((packet[17] & 0xF0) << 4 | packet[15]) * FRSKY_D_CHANNEL_SCALING);
-
-  for (int i = 0; i < FRSKY_D_CHANNEL_COUNT; i++) {
-    if ((channels[i] < 800) || (channels[i] > 2200)) {
-      failsafe = 1;
-      return;
-    }
-  }
+  channels[0] = (uint16_t)(((packet[10] & 0x0F) << 8 | packet[6]));
+  channels[1] = (uint16_t)(((packet[10] & 0xF0) << 4 | packet[7]));
+  channels[2] = (uint16_t)(((packet[11] & 0x0F) << 8 | packet[8]));
+  channels[3] = (uint16_t)(((packet[11] & 0xF0) << 4 | packet[9]));
+  channels[4] = (uint16_t)(((packet[16] & 0x0F) << 8 | packet[12]));
+  channels[5] = (uint16_t)(((packet[16] & 0xF0) << 4 | packet[13]));
+  channels[6] = (uint16_t)(((packet[17] & 0x0F) << 8 | packet[14]));
+  channels[7] = (uint16_t)(((packet[17] & 0xF0) << 4 | packet[15]));
 
   // if we made it this far, data is ready
   rx_ready = 1;
   failsafe = 0;
 
   // AETR channel order
-  rx[0] = channels[0] - 1499;
-  rx[1] = channels[1] - 1499;
-  rx[2] = channels[3] - 1499;
-  rx[3] = channels[2] - 999;
+  rx[0] = (channels[0] - 1500) - 750;
+  rx[1] = (channels[1] - 1500) - 750;
+  rx[2] = (channels[3] - 1500) - 750;
+  rx[3] = channels[2] - 1500;
 
   for (int i = 0; i < 3; i++) {
-    rx[i] *= 1.f / 500.f;
+    rx[i] *= 1.f / 750.f;
   }
-  rx[3] *= 1.f / 1000.f;
+  rx[3] *= 1.f / 1500.f;
 
   if (rx[3] > 1)
     rx[3] = 1;
@@ -633,13 +626,11 @@ void rx_init(void) {
 void checkrx() {
   switch (protocol_state) {
   case STATE_DETECT:
-    usb_debug_print("FRSKY STATE_DETECT\r\n");
     if (frsky_dectect()) {
       protocol_state = STATE_INIT;
     }
     break;
   case STATE_INIT:
-    usb_debug_print("FRSKY STATE_INIT\r\n");
     cc2500_enter_rxmode();
     cc2500_strobe(CC2500_SRX);
     if (frsky_bind.idx == 0xff) {
@@ -649,13 +640,11 @@ void checkrx() {
     }
     break;
   case STATE_BIND:
-    usb_debug_print("FRSKY STATE_BIND\r\n");
     init_tune_rx();
     protocol_state = STATE_BIND_TUNING;
     break;
   case STATE_BIND_TUNING:
     if (tune_rx(packet)) {
-      usb_debug_print("FRSKY STATE_BIND_TUNING DONE\r\n");
       set_address(1);
       init_get_bind();
       protocol_state = STATE_BIND_BINDING1;
@@ -663,28 +652,16 @@ void checkrx() {
     break;
   case STATE_BIND_BINDING1:
     if (get_bind1(packet)) {
-      usb_debug_print("FRSKY STATE_BIND_BINDING1 DONE\r\n");
       protocol_state = STATE_BIND_BINDING2;
     }
     break;
   case STATE_BIND_BINDING2:
     if (get_bind2(packet)) {
-      usb_debug_print("FRSKY STATE_BIND_BINDING2 DONE\r\n");
       protocol_state = STATE_BIND_COMPLETE;
     }
     break;
   case STATE_BIND_COMPLETE:
     cc2500_strobe(CC2500_SIDLE);
-    usb_debug_print("FRSKY STATE_BIND_COMPLETE\r\n");
-    usb_debug_printf("idx: %d\r\n", frsky_bind.idx);
-    usb_debug_printf("offset: %d\r\n", frsky_bind.offset);
-    usb_debug_printf("tx_id: 0x%x%x\r\n", frsky_bind.tx_id[0], frsky_bind.tx_id[1]);
-    usb_debug_printf("hop_data:");
-    for (uint32_t i = 0; i < 50; i++) {
-      usb_debug_printf(" %x, ", frsky_bind.hop_data[i]);
-    }
-    usb_debug_print("\r\n");
-
     rxmode = RXMODE_NORMAL;
     protocol_state = STATE_STARTING;
     break;
