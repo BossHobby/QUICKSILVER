00b1c11fbf9fd6cda0b3e5264ff6a681b3ceeb50
bkleiner <b.kleiner@str8labs.com>
2019-08-05 22:36:35 +0200

    add saner defaults to config

--- QUICKSILVER/src/main/config.h
+++ QUICKSILVER/src/main/config.h
@@ -70,7 +70,7 @@
 //***********************************************RECEIVER SETTINGS******************************************************
 
 // *************Receiver protocol selection									//todo:  add missing radio protocols from bobnova and make them all jive with new rx_init function in drv_rx_serial.c
-//#define RX_SBUS
+#define RX_SBUS
 //#define RX_CRSF                                           //Requires tbs firmware v2.88 or newer for failsafe to operate properly
 //#define RX_IBUS
 //#define RX_FPORT
@@ -79,11 +79,11 @@
 //#define RX_NRF24_BAYANG_TELEMETRY
 //#define RX_BAYANG_PROTOCOL_BLE_BEACON
 //#define RX_BAYANG_PROTOCOL_TELEMETRY_AUTOBIND
-#define RX_FRSKY
+//#define RX_FRSKY
 
 // *************Serial Receiver UART Selection (uncomment all if using spi receiver)																		//todo:  Many missing usart AF setups, So plenty more to do here
 //#define UART_1
-//#define UART_2
+#define UART_2
 //#define UART_3
 //#define UART_4
 //#define UART_6
