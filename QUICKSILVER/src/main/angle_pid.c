c426efec31030481227914c09b22c9ffd21c67bc
bkleiner <b.kleiner@str8labs.com>
2019-08-29 22:19:34 +0200

    format angle_pid

--- QUICKSILVER/src/main/angle_pid.c
+++ QUICKSILVER/src/main/angle_pid.c
@@ -1,33 +1,26 @@
 
-#include <stdbool.h>
-#include "pid.h"
-#include "util.h"
 #include "defines.h"
 #include "math.h"
+#include "pid.h"
+#include "util.h"
+#include <stdbool.h>
 
 #define APIDNUMBER 2
 
 //**************************** ANGLE PIDS - used in level mode to set leveling strength
 
-
-
-
-//NotFastEnuf dynamic angle mode test settings 
+//NotFastEnuf dynamic angle mode test settings
 
 // Leveling algorithm coefficients for small errors  (normal flying)
-float apidkp1[APIDNUMBER] = { 10.00 };  // P TERM GAIN ROLL + PITCH 
-float apidkd1[APIDNUMBER] = { 3.0 };    // D TERM GAIN ROLL + PITCH
+float apidkp1[APIDNUMBER] = {10.00}; // P TERM GAIN ROLL + PITCH
+float apidkd1[APIDNUMBER] = {3.0};   // D TERM GAIN ROLL + PITCH
 
 // Leveling algorithm coefficients for large errors  (stick banging or collisions)
-float apidkp2[APIDNUMBER] = { 5.00 };   // P TERM GAIN ROLL + PITCH 
-float apidkd2[APIDNUMBER] = { 0.0 };    // D TERM GAIN ROLL + PITCH
-
-
-
+float apidkp2[APIDNUMBER] = {5.00}; // P TERM GAIN ROLL + PITCH
+float apidkd2[APIDNUMBER] = {0.0};  // D TERM GAIN ROLL + PITCH
 
 // code variables below
-
-#define OUTLIMIT_FLOAT (apidkp1[0]+apidkp2[0])   //set angle pid output limit to sum of both P terms just in case
+#define OUTLIMIT_FLOAT (apidkp1[0] + apidkp2[0]) //set angle pid output limit to sum of both P terms just in case
 
 float apidoutput1[APIDNUMBER];
 float apidoutput2[APIDNUMBER];
@@ -35,28 +28,25 @@ float angleerror[APIDNUMBER];
 float lasterror[APIDNUMBER];
 float apidoutput[APIDNUMBER];
 
+float apid(int x) {
+  // P term 1 weighted
+  apidoutput1[x] = (1 - fabsf(angleerror[x])) * angleerror[x] * apidkp1[0];
 
-float apid(int x)
-{
+  // P term 2 weighted
+  apidoutput2[x] = fabsf(angleerror[x]) * angleerror[x] * apidkp2[0];
 
-	// P term 1 weighted
-	apidoutput1[x] = (1-fabsf(angleerror[x])) * angleerror[x] * apidkp1[0] ;
-	
-	// P term 2 weighted
-	apidoutput2[x] = fabsf(angleerror[x]) * angleerror[x] * apidkp2[0];  
-	
-extern float timefactor;
-	// D term 1 weighted + P term 1 weighted
-	apidoutput1[x] += (angleerror[x] - lasterror[x]) * apidkd1[0] * (1-fabsf(angleerror[x])) * timefactor;
-	
-	// D term 2 weighted + P term 2 weighted
-	apidoutput2[x] += ((angleerror[x] - lasterror[x]) * apidkd2[0] * fabsf(angleerror[x]) * timefactor);
-	
+  extern float timefactor;
+  // D term 1 weighted + P term 1 weighted
+  apidoutput1[x] += (angleerror[x] - lasterror[x]) * apidkd1[0] * (1 - fabsf(angleerror[x])) * timefactor;
+
+  // D term 2 weighted + P term 2 weighted
+  apidoutput2[x] += ((angleerror[x] - lasterror[x]) * apidkd2[0] * fabsf(angleerror[x]) * timefactor);
+
   // apidoutput sum
-	apidoutput[x] = apidoutput1[x] + apidoutput2[x];
-	
+  apidoutput[x] = apidoutput1[x] + apidoutput2[x];
+
   lasterror[x] = angleerror[x];
-	limitf(&apidoutput[x], OUTLIMIT_FLOAT);	
-	
-	return apidoutput[x];
+  limitf(&apidoutput[x], OUTLIMIT_FLOAT);
+
+  return apidoutput[x];
 }
--- QUICKSILVER/src/main/buzzer.h
+++ QUICKSILVER/src/main/buzzer.h
@@ -1,3 +1,3 @@
-void buzzer( void);
+#pragma once
 
-
+void buzzer(void);
