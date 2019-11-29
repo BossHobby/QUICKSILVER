7bafd6d80dd5d69611d6a604b2ba2a638985bd4e
bkleiner <b.kleiner@str8labs.com>
2019-11-29 23:28:33 +0100

    imu: refactor different imu filters

--- src/main/imu.c
+++ src/main/imu.c
@@ -74,42 +74,39 @@ void vectorcopy(float *vector1, float *vector2) {
 
 extern float looptime;
 
+#ifdef BFPV_IMU
 void imu_calc(void) {
   // remove bias and reduce to accel in G
   accel[0] = (accel[0] - accelcal[0]) * (1 / 2048.0f);
   accel[1] = (accel[1] - accelcal[1]) * (1 / 2048.0f);
   accel[2] = (accel[2] - accelcal[2]) * (1 / 2048.0f);
 
-#ifdef BFPV_IMU
   accel[0] = iir_filter_lpf2_apply(&accel_filter[0], accel[0]);
   accel[1] = iir_filter_lpf2_apply(&accel_filter[1], accel[1]);
   accel[2] = iir_filter_lpf2_apply(&accel_filter[2], accel[2]);
 
   float EstG[3];
-
   vectorcopy(&EstG[0], &GEstG[0]);
 
   float gyros[3];
   for (int i = 0; i < 3; i++) {
     gyros[i] = gyro[i] * looptime;
   }
+
+  const float cosx = _cosf(gyros[1]);
+  const float sinx = _sinf(gyros[1]);
+  const float cosy = _cosf(gyros[0]);
+  const float siny = _sinf(-gyros[0]);
+  const float cosz = _cosf(gyros[2]);
+  const float sinz = _sinf(-gyros[2]);
+
+  const float coszcosx = cosz * cosx;
+  const float coszcosy = cosz * cosy;
+  const float sinzcosx = sinz * cosx;
+  const float coszsinx = sinx * cosz;
+  const float sinzsinx = sinx * sinz;
+
   float mat[3][3];
-  float cosx, sinx, cosy, siny, cosz, sinz;
-  float coszcosx, coszcosy, sinzcosx, coszsinx, sinzsinx;
-
-  cosx = _cosf(gyros[1]);
-  sinx = _sinf(gyros[1]);
-  cosy = _cosf(gyros[0]);
-  siny = _sinf(-gyros[0]);
-  cosz = _cosf(gyros[2]);
-  sinz = _sinf(-gyros[2]);
-
-  coszcosx = cosz * cosx;
-  coszcosy = cosz * cosy;
-  sinzcosx = sinz * cosx;
-  coszsinx = sinx * cosz;
-  sinzsinx = sinx * sinz;
-
   mat[0][0] = coszcosy;
   mat[0][1] = -cosy * sinz;
   mat[0][2] = siny;
@@ -125,22 +122,6 @@ void imu_calc(void) {
   EstG[2] = GEstG[0] * mat[0][2] + GEstG[1] * mat[1][2] + GEstG[2] * mat[2][2];
 
   vectorcopy(&GEstG[0], &EstG[0]);
-#else
-  const float gyro_delta_angle[3] = {
-      gyro[0] * looptime,
-      gyro[1] * looptime,
-      gyro[2] * looptime,
-  };
-
-  GEstG[2] = GEstG[2] - (gyro_delta_angle[0]) * GEstG[0];
-  GEstG[0] = (gyro_delta_angle[0]) * GEstG[2] + GEstG[0];
-
-  GEstG[1] = GEstG[1] + (gyro_delta_angle[1]) * GEstG[2];
-  GEstG[2] = -(gyro_delta_angle[1]) * GEstG[1] + GEstG[2];
-
-  GEstG[0] = GEstG[0] - (gyro_delta_angle[2]) * GEstG[1];
-  GEstG[1] = (gyro_delta_angle[2]) * GEstG[0] + GEstG[1];
-#endif
 
   extern int onground;
   if (onground) { //happyhour bartender - quad is ON GROUND and disarmed
@@ -158,7 +139,6 @@ void imu_calc(void) {
       }
     }
   } else {
-#ifdef BFPV_IMU
     float accmag = calcmagnitude(&accel[0]);
     for (int axis = 0; axis < 3; axis++) {
       accel[axis] = accel[axis] * (ACC_1G / accmag);
@@ -167,7 +147,57 @@ void imu_calc(void) {
     for (int x = 0; x < 3; x++) {
       lpf(&GEstG[x], accel[x], filtcoeff);
     }
+
+    //heal the gravity vector after fusion with accel
+    float GEstGmag = calcmagnitude(&GEstG[0]);
+    for (int axis = 0; axis < 3; axis++) {
+      GEstG[axis] = GEstG[axis] * (ACC_1G / GEstGmag);
+    }
+  }
+
+  if (rx_aux_on(AUX_HORIZON)) {
+    attitude[0] = atan2approx(GEstG[0], GEstG[2]);
+    attitude[1] = atan2approx(GEstG[1], GEstG[2]);
+  }
+}
 #else
+void imu_calc(void) {
+  // remove bias and reduce to accel in G
+  accel[0] = (accel[0] - accelcal[0]) * (1 / 2048.0f);
+  accel[1] = (accel[1] - accelcal[1]) * (1 / 2048.0f);
+  accel[2] = (accel[2] - accelcal[2]) * (1 / 2048.0f);
+
+  const float gyro_delta_angle[3] = {
+      gyro[0] * looptime,
+      gyro[1] * looptime,
+      gyro[2] * looptime,
+  };
+
+  GEstG[2] = GEstG[2] - (gyro_delta_angle[0]) * GEstG[0];
+  GEstG[0] = (gyro_delta_angle[0]) * GEstG[2] + GEstG[0];
+
+  GEstG[1] = GEstG[1] + (gyro_delta_angle[1]) * GEstG[2];
+  GEstG[2] = -(gyro_delta_angle[1]) * GEstG[1] + GEstG[2];
+
+  GEstG[0] = GEstG[0] - (gyro_delta_angle[2]) * GEstG[1];
+  GEstG[1] = (gyro_delta_angle[2]) * GEstG[0] + GEstG[1];
+
+  extern int onground;
+  if (onground) { //happyhour bartender - quad is ON GROUND and disarmed
+    // calc acc mag
+    float accmag = calcmagnitude(&accel[0]);
+    if ((accmag > ACC_MIN * ACC_1G) && (accmag < ACC_MAX * ACC_1G)) {
+      // normalize acc
+      for (int axis = 0; axis < 3; axis++) {
+        accel[axis] = accel[axis] * (ACC_1G / accmag);
+      }
+
+      float filtcoeff = lpfcalc_hz(looptime, 1.0f / (float)FASTFILTER);
+      for (int x = 0; x < 3; x++) {
+        lpf(&GEstG[x], accel[x], filtcoeff);
+      }
+    }
+  } else {
     //lateshift bartender - quad is IN AIR and things are getting wild
     // hit accel[3] with a sledgehammer
 #ifdef PREFILTER
@@ -190,7 +220,6 @@ void imu_calc(void) {
         lpf(&GEstG[x], accel[x], filtcoeff);
       }
     }
-#endif
     //heal the gravity vector after fusion with accel
     float GEstGmag = calcmagnitude(&GEstG[0]);
     for (int axis = 0; axis < 3; axis++) {
@@ -203,3 +232,4 @@ void imu_calc(void) {
     attitude[1] = atan2approx(GEstG[1], GEstG[2]);
   }
 }
+#endif
