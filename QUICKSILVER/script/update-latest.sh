f70c20913a4e2563c2413e257af0bc0ac1efc41e
bkleiner <b.kleiner@str8labs.com>
2019-12-08 12:38:32 +0100

    add script to update latest tag

---
+++ QUICKSILVER/script/update-latest.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+TAG_NAME="latest"
+
+git push origin :refs/tags/$TAG_NAME
+git tag -fa -m "$TAG_NAME" $TAG_NAME
+git push origin master --tags
+
+hub release -f "%T (%S) %n" --include-drafts | grep " (draft)" | awk '{print $1}' | xargs -t -n1 hub release delete
