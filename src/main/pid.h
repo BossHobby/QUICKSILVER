0a6316727cb780e430a4cfb6d31a80b55811f632
bkleiner <b.kleiner@str8labs.com>
2019-12-14 10:54:16 +0100

    format pid.h

--- src/main/pid.h
+++ src/main/pid.h
@@ -1,10 +1,11 @@
+#pragma once
+
 void rotateErrors(void);
-float pid( int x );
-int next_pid_term( void); // Return value : 0 - p, 1 - i, 2 - d
-int next_pid_axis( void); // Return value : 0 - Roll, 1 - Pitch, 2 - Yaw
-int increase_pid( void );
-int decrease_pid( void );
-void pid_precalc( void);
 
+void pid_precalc(void);
+float pid(int x);
 
-
+int next_pid_term(void); // Return value : 0 - p, 1 - i, 2 - d
+int next_pid_axis(void); // Return value : 0 - Roll, 1 - Pitch, 2 - Yaw
+int increase_pid(void);
+int decrease_pid(void);
