c3d4be487814f5d4fee0702eec98c7439d05b19f
bkleiner <b.kleiner@str8labs.com>
2020-01-01 20:13:15 +0100

    update-latest: check if hub exists first

--- script/update-latest.sh
+++ script/update-latest.sh
@@ -5,4 +5,6 @@ git push origin :refs/tags/$TAG_NAME
 git tag -fa -m "$TAG_NAME" $TAG_NAME
 git push origin master --tags
 
-hub release -f "%T (%S) %n" --include-drafts | grep " (draft)" | awk '{print $1}' | xargs -t -n1 hub release delete
+if which hub; then 
+  hub release -f "%T (%S) %n" --include-drafts | grep " (draft)" | awk '{print $1}' | xargs -t -n1 hub release delete
+fi
