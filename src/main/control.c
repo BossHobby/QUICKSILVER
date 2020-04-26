f2d55a45d936ca6482b638bab3033af904e4b314
bkleiner <b.kleiner@str8labs.com>
2020-04-26 13:09:31 +0200

    control: format arming logic be as verbose as possible

--- src/main/control.c
+++ src/main/control.c
@@ -247,26 +247,44 @@ void control(void) {
     pid(2);
   }
 
-  //this is needed for osd warnings but could be better used below too
-  if (((rx[3] > THROTTLE_SAFETY) && (arming_release == 0)) && rx_aux_on(AUX_ARMING)) {
-    throttle_safety = 1;
-  } else {
-    throttle_safety = 0;
-  }
+  // CONDITION: switch is ARMED
+  if (rx_aux_on(AUX_ARMING)) {
+    // CONDITION: throttle is above safety limit and ARMING RELEASE FLAG IS NOT CLEARED
+    if ((rx[3] > THROTTLE_SAFETY) && (arming_release == 0)) {
+      throttle_safety = 1;
+    } else {
+      throttle_safety = 0;
+    }
 
-  if (!rx_aux_on(AUX_ARMING)) { // 						CONDITION: switch is DISARMED
-    armed_state = 0;            // 												disarm the quad by setting armed state variable to zero
-    if (rx_ready == 1)
-      binding_while_armed = 0;                                                                //                        rx is bound and has been disarmed so clear binding while armed flag
-  } else {                                                                                    // 						CONDITION: switch is ARMED
-    if (((rx[3] > THROTTLE_SAFETY) && (arming_release == 0)) || (binding_while_armed == 1)) { //				   CONDITION: (throttle is above safety limit and ARMING RELEASE FLAG IS NOT CLEARED) OR (bind just took place with transmitter armed)
-      armed_state = 0;                                                                        //                         	 				override to disarmed state and rapid blink the leds
+    // CONDITION: (throttle is above safety limit and ARMING RELEASE FLAG IS NOT CLEARED) OR (bind just took place with transmitter armed)
+    if ((throttle_safety == 1) || (binding_while_armed == 1)) {
+      // override to disarmed state
+      armed_state = 0;
+
+      // rapid blink the leds
       ledcommand = 1;
-    } else {              //            					 CONDITION: quad is being armed in a safe state
-      armed_state = 1;    //                      					  arm the quad by setting armed state variable to 1
-      arming_release = 1; //                       						clear the arming release flag - the arming release flag being cleared
-    }                     //											 						is what stops the quad from automatically disarming again the next time
-  }                       //											 						throttle is raised above the safety limit
+    } else {
+      // CONDITION: quad is being armed in a safe state
+
+      // arm the quad by setting armed state variable to 1
+      armed_state = 1;
+
+      // clear the arming release flag - the arming release flag being cleared
+      // is what stops the quad from automatically disarming again the next time
+      // throttle is raised above the safety limit
+      arming_release = 1;
+    }
+  } else {
+    // CONDITION: switch is DISARMED
+
+    // disarm the quad by setting armed state variable to zero
+    armed_state = 0;
+
+    if (rx_ready == 1) {
+      // rx is bound and has been disarmed so clear binding while armed flag
+      binding_while_armed = 0;
+    }
+  }
 
   if (!rx_aux_on(AUX_IDLE_UP)) {
     idle_state = 0;
@@ -274,24 +292,47 @@ void control(void) {
     idle_state = 1;
   }
 
-  if (armed_state == 0) {  // CONDITION: armed state variable is 0 so quad is DISARMED
-    throttle = 0;          //						override throttle to 0
-    in_air = 0;            //						flag in air variable as NOT IN THE AIR for mix throttle increase safety
-    arming_release = 0;    //						arming release flag is set to not cleared to reactivate the throttle safety limit for the next arming event
-  } else {                 // CONDITION: armed state variable is 1 so quad is ARMED
-    if (idle_state == 0) { //            CONDITION: idle up is turned OFF
+  // CONDITION: armed state variable is 0 so quad is DISARMED
+  if (armed_state == 0) {
+    // override throttle to 0
+    throttle = 0;
+
+    // flag in air variable as NOT IN THE AIR for mix throttle increase safety
+    in_air = 0;
+
+    // arming release flag is set to not cleared to reactivate the throttle safety limit for the next arming event
+    arming_release = 0;
+  } else {
+    // CONDITION: armed state variable is 1 so quad is ARMED
+
+    if (idle_state == 0) {
+      // CONDITION: idle up is turned OFF
+
       if (rx[3] < 0.05f) {
-        throttle = 0; //   											set a small dead zone where throttle is zero and
-        in_air = 0;   //												deactivate mix increase 3 since throttle is off
+        // set a small dead zone where throttle is zero and
+        throttle = 0;
+
+        // deactivate mix increase 3 since throttle is off
+        in_air = 0;
       } else {
-        throttle = (rx[3] - 0.05f) * 1.05623158f; //                        map the remainder of the the active throttle region to 100%
+        // map the remainder of the the active throttle region to 100%
+        throttle = (rx[3] - 0.05f) * 1.05623158f;
+
+        // activate mix increase since throttle is on
         in_air = 1;
-      }                                                              //												activate mix increase since throttle is on
-    } else {                                                         //						CONDITION: idle up is turned ON
-      throttle = (float)IDLE_THR + rx[3] * (1.0f - (float)IDLE_THR); //            						throttle range is mapped from idle throttle value to 100%
-      if ((rx[3] > THROTTLE_SAFETY) && (in_air == 0))
-        in_air = 1; //            						change the state of in air flag when first crossing the throttle
-    }               //            						safety value to indicate craft has taken off for mix increase safety
+      }
+    } else {
+      // CONDITION: idle up is turned ON
+
+      // throttle range is mapped from idle throttle value to 100%
+      throttle = (float)IDLE_THR + rx[3] * (1.0f - (float)IDLE_THR);
+
+      if ((rx[3] > THROTTLE_SAFETY) && (in_air == 0)) {
+        // change the state of in air flag when first crossing the throttle
+        // safety value to indicate craft has taken off for mix increase safety
+        in_air = 1;
+      }
+    }
   }
 
 #ifdef STICK_TRAVEL_CHECK //This feature completely disables throttle and allows visual feedback if control inputs reach full throws
@@ -303,6 +344,7 @@ void control(void) {
     }
   }
 #endif
+
   if (usb_motor_test.active) {
     float mix[4] = {0, 0, 0, 0};
     motor_mixer_calc(mix);
