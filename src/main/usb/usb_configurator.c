4af8e73ffd0d66613f969e2edb511f8ffb63bb9f
bkleiner <b.kleiner@str8labs.com>
2020-05-24 14:42:05 +0200

    usb: remove old debug commands

--- src/main/usb/usb_configurator.c
+++ src/main/usb/usb_configurator.c
@@ -51,59 +51,6 @@ void usb_configurator(void) {
   case USB_MAGIC_QUIC:
     usb_process_quic();
     break;
-#ifdef DEBUG
-  case 'D':
-    usb_serial_printf("gyroid: %x\r\n", debug.gyroid);
-    usb_serial_printf("adcfilt: %f\r\n", debug.adcfilt);
-    usb_serial_printf("looptime: %f\r\n", debug.timefilt);
-    usb_serial_printf("cpu_load: %f\r\n", debug.cpu_load * 100);
-    usb_serial_printf("max_cpu_load: %f\r\n", debug.max_cpu_load * 100);
-    usb_serial_printf("RX: %f %f %f %f\r\n", state.rx.axis[0], state.rx.axis[1], state.rx.axis[2], state.rx.axis[3]);
-#ifdef RX_FRSKY
-    usb_serial_print("FRSKY_BIND\r\n");
-    usb_serial_printf("idx: %d\r\n", frsky_bind.idx);
-    usb_serial_printf("offset: %d\r\n", frsky_bind.offset);
-    usb_serial_printf("tx_id: 0x%x%x\r\n", frsky_bind.tx_id[0], frsky_bind.tx_id[1]);
-    usb_serial_printf("hop_data:");
-    for (uint32_t i = 0; i < 50; i++) {
-      usb_serial_printf(" %x, ", frsky_bind.hop_data[i]);
-    }
-    usb_serial_print("\r\n");
-#endif
-    break;
-#endif
-  case 'P':
-    if (profile.rate.mode == RATE_MODE_BETAFLIGHT) {
-      usb_serial_printf("betaflight ratemode\r\n");
-      usb_serial_printf("rc_rate: %f %f %f\r\n",
-                        profile.rate.betaflight.rc_rate.roll,
-                        profile.rate.betaflight.rc_rate.pitch,
-                        profile.rate.betaflight.rc_rate.yaw);
-      usb_serial_printf("super_rate: %f %f %f\r\n",
-                        profile.rate.betaflight.super_rate.roll,
-                        profile.rate.betaflight.super_rate.pitch,
-                        profile.rate.betaflight.super_rate.yaw);
-    } else {
-      usb_serial_printf("silverware ratemode\r\n");
-      usb_serial_printf("max_rate: %f %f %f\r\n",
-                        profile.rate.silverware.max_rate.roll,
-                        profile.rate.silverware.max_rate.pitch,
-                        profile.rate.silverware.max_rate.yaw);
-    }
-
-    usb_serial_printf("pidkp: %f %f %f\r\n",
-                      profile_current_pid_rates()->kp.axis[0],
-                      profile_current_pid_rates()->kp.axis[1],
-                      profile_current_pid_rates()->kp.axis[2]);
-    usb_serial_printf("pidki: %f %f %f\r\n",
-                      profile_current_pid_rates()->ki.axis[0],
-                      profile_current_pid_rates()->ki.axis[1],
-                      profile_current_pid_rates()->ki.axis[2]);
-    usb_serial_printf("pidkd: %f %f %f\r\n",
-                      profile_current_pid_rates()->kd.axis[0],
-                      profile_current_pid_rates()->kd.axis[1],
-                      profile_current_pid_rates()->kd.axis[2]);
-    break;
   }
 
   // this will block and handle all usb traffic while active
