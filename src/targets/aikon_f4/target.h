2df9183fb08d253fef784e9dec0d629355608474
bkleiner <b.kleiner@str8labs.com>
2020-06-13 00:29:27 +0200

    add aikon f4 target

---
+++ src/targets/aikon_f4/target.h
@@ -0,0 +1,90 @@
+#include "config.h"
+#include "config_helper.h"
+
+#define AikonF4
+
+#define F4
+#define F405
+
+//PORTS
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7  \
+  SPI2_PB13B14B15 \
+  SPI3_PC10PC11PC12
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2     \
+  USART3_PB11PB10   \
+  USART4_PA1PA0
+
+// #define USB_DETECT_PIN GPIO_Pin_2
+// #define USB_DETECT_PORT GPIOD
+
+//LEDS
+#define LED_NUMBER 1
+#define LED1PIN GPIO_Pin_4
+#define LED1PORT GPIOB
+#define LED1_INVERT
+//#define BUZZER_PIN GPIO_Pin_4
+//#define BUZZER_PIN_PORT GPIOB
+//#define BUZZER_INVERT
+//#define FPV_PIN GPIO_Pin_13
+//#define FPV_PORT GPIOA
+
+//GYRO
+#define MPU6XXX_SPI_PORT SPI_PORT1
+#define MPU6XXX_NSS PIN_A4
+#define MPU6XXX_INT PIN_C4
+#define USE_DUMMY_I2C
+// #define DISABLE_GYRO_CHECK
+#define GYRO_ID_1 0x12
+#define GYRO_ID_2 0xaf
+#define GYRO_ID_3 0xac
+#define GYRO_ID_4 0x98
+#define SENSOR_ROTATE_90_CW
+
+//RADIO
+#define USART_INVERTER_PIN GPIO_Pin_0 //UART 1
+#define USART_INVERTER_PORT GPIOC
+
+#ifdef SERIAL_RX
+#define SOFTSPI_NONE
+#define RX_USART USART_PORT1
+#endif
+#ifndef SOFTSPI_NONE
+#define RADIO_CHECK
+#define SPI_MISO_PIN GPIO_Pin_1
+#define SPI_MISO_PORT GPIOA
+#define SPI_MOSI_PIN GPIO_Pin_4
+#define SPI_MOSI_PORT GPIOB
+#define SPI_CLK_PIN GPIO_Pin_10
+#define SPI_CLK_PORT GPIOA
+#define SPI_SS_PIN GPIO_Pin_6
+#define SPI_SS_PORT GPIOB
+#endif
+
+//OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT3
+#define MAX7456_NSS PIN_A15
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN GPIO_Pin_2
+#define BATTERYPORT GPIOC
+#define BATTERY_ADC_CHANNEL ADC_Channel_12
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+#define MOTOR_PIN0 MOTOR_PIN_PC8
+#define MOTOR_PIN1 MOTOR_PIN_PC9
+#define MOTOR_PIN2 MOTOR_PIN_PC6
+#define MOTOR_PIN3 MOTOR_PIN_PC7
