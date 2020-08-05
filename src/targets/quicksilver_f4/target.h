cb3808af65a659b813718d895b17c88db6e5faed
NotFastEnuf <schrock7903@gmail.com>
2020-08-04 22:24:57 -0400

    added nfe_quicksilver target

---
+++ src/targets/quicksilver_f4/target.h
@@ -0,0 +1,84 @@
+#include "config.h"
+#include "config_helper.h"
+
+#define F4
+#define F405
+#define Quicksilver_F4
+
+//PORTS			//pa15 nss on spi3
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7  \
+  SPI2_PB13B14B15 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2     \
+  USART3_PC11PC10   \
+  USART4_PA1PA0
+
+//LEDS
+#define LED_NUMBER 1
+#define LED1PIN GPIO_Pin_12
+#define LED1PORT GPIOC
+#define LED1_INVERT
+#define BUZZER_PIN GPIO_Pin_14
+#define BUZZER_PIN_PORT GPIOA
+#define BUZZER_INVERT
+#define FPV_PIN GPIO_Pin_13
+#define FPV_PORT GPIOC
+
+//GYRO
+#define ICM20602_SPI_PORT SPI_PORT1
+#define ICM20602_NSS PIN_A4
+#define ICM20602_INT PIN_C14
+#define USE_DUMMY_I2C
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x12
+#define GYRO_ID_3 0x69
+#define GYRO_ID_4 0x71
+#define SENSOR_FLIP_180
+#define SENSOR_ROTATE_90_CW
+
+//RADIO
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT1
+#define SOFTSPI_NONE
+#endif
+
+#ifndef SOFTSPI_NONE
+#define RADIO_CHECK
+#define SPI_MISO_PIN GPIO_Pin_10
+#define SPI_MISO_PORT GPIOA
+#define SPI_MOSI_PIN GPIO_Pin_9
+#define SPI_MOSI_PORT GPIOA
+#define SPI_CLK_PIN GPIO_Pin_11
+#define SPI_CLK_PORT GPIOC
+#define SPI_SS_PIN GPIO_Pin_10
+#define SPI_SS_PORT GPIOC
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN GPIO_Pin_2
+#define BATTERYPORT GPIOC
+#define BATTERY_ADC_CHANNEL ADC_Channel_12
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+#define MOTOR_PIN0 MOTOR_PIN_PC7
+#define MOTOR_PIN1 MOTOR_PIN_PC6
+#define MOTOR_PIN2 MOTOR_PIN_PC9
+#define MOTOR_PIN3 MOTOR_PIN_PC8
