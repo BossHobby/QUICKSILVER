831d9d7c19d9a37abe7a7ae3bf7fc7c9de5831a9
bkleiner <b.kleiner@str8labs.com>
2020-09-27 11:19:31 +0200

    drv_serial_rx: use switch

--- src/drivers/drv_serial_rx.c
+++ src/drivers/drv_serial_rx.c
@@ -206,71 +206,114 @@ void serial_rx_init(rx_serial_protocol_t rx_serial_protocol) {
 
   serial_rx_port = profile.serial.rx;
 
-  GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_DSM || rx_serial_protocol == RX_SERIAL_PROTOCOL_IBUS || rx_serial_protocol == RX_SERIAL_PROTOCOL_CRSF) {
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-  } else if (rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS ||
-             rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS_INVERTED ||
-             rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT ||
-             rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT_INVERTED ||
-             rx_serial_protocol == RX_SERIAL_PROTOCOL_REDPINE ||
-             rx_serial_protocol == RX_SERIAL_PROTOCOL_REDPINE_INVERTED) {
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitTypeDef gpio_init;
+  gpio_init.GPIO_Mode = GPIO_Mode_AF;
+  gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
+
+  switch (rx_serial_protocol) {
+  case RX_SERIAL_PROTOCOL_DSM:
+  case RX_SERIAL_PROTOCOL_IBUS:
+  case RX_SERIAL_PROTOCOL_CRSF:
+    gpio_init.GPIO_Pin = USART.rx_pin.pin;
+    gpio_init.GPIO_OType = GPIO_OType_OD;
+    gpio_init.GPIO_PuPd = GPIO_PuPd_UP;
+
+    GPIO_Init(USART.rx_pin.port, &gpio_init);
+    GPIO_PinAFConfig(USART.rx_pin.port, USART.rx_pin.pin_source, USART.gpio_af);
+    break;
+
+  case RX_SERIAL_PROTOCOL_SBUS:
+  case RX_SERIAL_PROTOCOL_SBUS_INVERTED:
+  case RX_SERIAL_PROTOCOL_FPORT:
+  case RX_SERIAL_PROTOCOL_FPORT_INVERTED:
+  case RX_SERIAL_PROTOCOL_REDPINE:
+  case RX_SERIAL_PROTOCOL_REDPINE_INVERTED:
+    gpio_init.GPIO_Pin = USART.rx_pin.pin;
+    gpio_init.GPIO_OType = GPIO_OType_PP;
+    gpio_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+    GPIO_Init(USART.rx_pin.port, &gpio_init);
+    GPIO_PinAFConfig(USART.rx_pin.port, USART.rx_pin.pin_source, USART.gpio_af);
+    break;
+
+  default:
+    // no rx-pin? maybe throw error
+    break;
   }
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+  switch (rx_serial_protocol) {
+  case RX_SERIAL_PROTOCOL_FPORT:
+  case RX_SERIAL_PROTOCOL_FPORT_INVERTED:
+  case RX_SERIAL_PROTOCOL_CRSF:
+    gpio_init.GPIO_Pin = USART.tx_pin.pin;
+    GPIO_Init(USART.tx_pin.port, &gpio_init);
+
+    GPIO_PinAFConfig(USART.tx_pin.port, USART.tx_pin.pin_source, USART.gpio_af);
+    break;
+
+  default:
+    // no tx pin for the others
+    break;
+  }
 
   serial_enable_rcc(serial_rx_port);
 
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT || rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT_INVERTED) {
-    GPIO_InitStructure.GPIO_Pin = USART.tx_pin.pin;
-    GPIO_Init(USART.tx_pin.port, &GPIO_InitStructure);
+  USART_InitTypeDef usart_init;
+  usart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  usart_init.USART_WordLength = USART_WordLength_8b;
 
-    GPIO_PinAFConfig(USART.tx_pin.port, USART.tx_pin.pin_source, USART.gpio_af);
+  switch (rx_serial_protocol) {
+  case RX_SERIAL_PROTOCOL_DSM:
+  case RX_SERIAL_PROTOCOL_IBUS:
+    usart_init.USART_BaudRate = 115200;
+    usart_init.USART_StopBits = USART_StopBits_1;
+    usart_init.USART_Parity = USART_Parity_No;
+    usart_init.USART_Mode = USART_Mode_Rx;
+    break;
+
+  case RX_SERIAL_PROTOCOL_FPORT:
+  case RX_SERIAL_PROTOCOL_FPORT_INVERTED:
+    usart_init.USART_BaudRate = 115200;
+    usart_init.USART_StopBits = USART_StopBits_1;
+    usart_init.USART_Parity = USART_Parity_No;
+    usart_init.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    break;
+
+  case RX_SERIAL_PROTOCOL_SBUS:
+  case RX_SERIAL_PROTOCOL_SBUS_INVERTED:
+    usart_init.USART_BaudRate = 100000;
+    usart_init.USART_StopBits = USART_StopBits_2;
+    usart_init.USART_Parity = USART_Parity_Even;
+    usart_init.USART_Mode = USART_Mode_Rx;
+    break;
+
+  case RX_SERIAL_PROTOCOL_CRSF:
+    usart_init.USART_BaudRate = 420000;
+    usart_init.USART_StopBits = USART_StopBits_1;
+    usart_init.USART_Parity = USART_Parity_No;
+    usart_init.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    break;
+
+  case RX_SERIAL_PROTOCOL_REDPINE:
+  case RX_SERIAL_PROTOCOL_REDPINE_INVERTED:
+    usart_init.USART_BaudRate = 230400;
+    usart_init.USART_StopBits = USART_StopBits_1;
+    usart_init.USART_Parity = USART_Parity_No;
+    usart_init.USART_Mode = USART_Mode_Rx;
+    break;
+
+  default:
+    break;
   }
 
-  GPIO_InitStructure.GPIO_Pin = USART.rx_pin.pin;
-  GPIO_Init(USART.rx_pin.port, &GPIO_InitStructure);
-
-  GPIO_PinAFConfig(USART.rx_pin.port, USART.rx_pin.pin_source, USART.gpio_af);
-
-  USART_InitTypeDef USART_InitStructure;
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_DSM || rx_serial_protocol == RX_SERIAL_PROTOCOL_IBUS || rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT || rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT_INVERTED) {
-    USART_InitStructure.USART_BaudRate = 115200;
-  } else if (rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS || rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS_INVERTED) {
-    USART_InitStructure.USART_BaudRate = 100000;
-  } else if (rx_serial_protocol == RX_SERIAL_PROTOCOL_CRSF) {
-    USART_InitStructure.USART_BaudRate = 420000;
-  } else if (rx_serial_protocol == RX_SERIAL_PROTOCOL_REDPINE || rx_serial_protocol == RX_SERIAL_PROTOCOL_REDPINE_INVERTED) {
-    USART_InitStructure.USART_BaudRate = 230400;
-  }
-
-  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS || rx_serial_protocol == RX_SERIAL_PROTOCOL_SBUS_INVERTED) {
-    USART_InitStructure.USART_StopBits = USART_StopBits_2;
-    USART_InitStructure.USART_Parity = USART_Parity_Even;
-  } else {
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-  }
-
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT || rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT_INVERTED || rx_serial_protocol == RX_SERIAL_PROTOCOL_CRSF) {
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-  } else {
-    USART_InitStructure.USART_Mode = USART_Mode_Rx; //USART_Mode_Rx | USART_Mode_Tx;
-  }
-
-  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT) { //RX_SERIAL_PROTOCOL_FPORT_INVERTED requires half duplex off) {
+  if (rx_serial_protocol == RX_SERIAL_PROTOCOL_FPORT) {
+    //RX_SERIAL_PROTOCOL_FPORT_INVERTED requires half duplex off
     USART_HalfDuplexCmd(USART.channel, ENABLE);
   } else {
     USART_HalfDuplexCmd(USART.channel, DISABLE);
   }
 
-  USART_Init(USART.channel, &USART_InitStructure);
+  USART_Init(USART.channel, &usart_init);
 
 #ifdef F0
 #ifdef INVERT_UART
