b48f2da5eabcfb094112dc89d41b488b5961446b
bkleiner <b.kleiner@str8labs.com>
2021-04-15 00:19:44 +0200

    add comment about the effects of motortest_override

--- src/main/control.h
+++ src/main/control.h
@@ -26,7 +26,7 @@ typedef struct {
 
   uint8_t controls_override : 1; // will activate rx_override below & will write directly to the motors (motor_test)
   uint8_t acro_override : 1;
-  uint8_t motortest_override : 1;
+  uint8_t motortest_override : 1; // tuns off digital idle in the dshot driver & will write either sticks or usb_motortest values directly to motors
   uint8_t turtle : 1;
 
   uint8_t usb_active : 1;
