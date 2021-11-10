91c3d3ad9ea3c5f01e5334272179b6fd79d5f5b5
Hallucino <hallucino@users.noreply.github.com>
2021-11-10 15:07:27 +0100

    Add iflight f411 rx target
    New bord: iflight f411 rx

---
+++ src/targets/iflightf411rx/target.h
@@ -0,0 +1,85 @@
+
+#include "config.h"
+#include "config_helper.h"
+
+#define IFLIGHTF411RX
+
+#define F4
+#define F411
+
+//PORTS
+#define SPI_PORTS   \
+  SPI1_PA5PA6PA7    \
+  SPI2_PB13PB14PB15 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2
+
+//LEDS
+#define LED_NUMBER 1
+#define LED1PIN PIN_C13
+#define LED1_INVERT
+#define BUZZER_PIN PIN_C15
+
+//#define FPV_PIN GPIO_Pin_13
+//#define FPV_PORT GPIOA
+
+//GYRO
+#define MPU6XXX_SPI_PORT SPI_PORT1
+#define MPU6XXX_NSS PIN_A4
+#define MPU6XXX_INT PIN_B10
+#define SENSOR_ROTATE_90_CCW
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x73
+#define GYRO_ID_3 0x78
+#define GYRO_ID_4 0x71
+
+//RADIO
+#ifdef RX_FRSKY
+#define USE_CC2500
+#define USE_CC2500_PA_LNA
+#define USE_CC2500_DIVERSITY
+#define CC2500_SPI_PORT SPI_PORT3
+#define CC2500_NSS PIN_A15
+#define CC2500_GDO0_PIN PIN_C14
+#define CC2500_TX_EN_PIN PIN_A8
+#define CC2500_LNA_EN_PIN PIN_NONE
+#define CC2500_ANT_SEL_PIN PIN_NONE
+
+#define SOFTSPI_NONE
+#endif
+
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#define SOFTSPI_NONE
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN PIN_B0
+#define BATTERY_ADC_CHANNEL ADC_Channel_8
+
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+
+// MOTOR PINS
+#define MOTOR_PIN0 MOTOR_PIN_PB6
+#define MOTOR_PIN1 MOTOR_PIN_PB7
+#define MOTOR_PIN2 MOTOR_PIN_PA0
+#define MOTOR_PIN3 MOTOR_PIN_PA1
