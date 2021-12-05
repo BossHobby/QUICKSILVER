514f6f2f71bbd758971628effe0fe5bd4ece0cea
Kevin Plaizier <46289813+Quick-Flash@users.noreply.github.com>
2021-12-04 18:48:17 -0700

    reduce angle pid calculations

--- src/main/angle_pid.c
+++ src/main/angle_pid.c
@@ -17,18 +17,21 @@ static float apidoutput[ANGLE_PID_SIZE];
 static float lasterror[ANGLE_PID_SIZE];
 
 float angle_pid(int x) {
+  float inverse_angle_error = 1 - fabsf(state.angleerror[x]);
+  float abs_angle_error = fabsf(state.angleerror[x]);
+
   // P term 1 weighted
-  apidoutput1[x] = (1 - fabsf(state.angleerror[x])) * state.angleerror[x] * profile.pid.small_angle.kp;
+  apidoutput1[x] = inverse_angle_error * state.angleerror[x] * profile.pid.small_angle.kp;
 
   // P term 2 weighted
-  apidoutput2[x] = fabsf(state.angleerror[x]) * state.angleerror[x] * profile.pid.big_angle.kp;
+  apidoutput2[x] = abs_angle_error * state.angleerror[x] * profile.pid.big_angle.kp;
 
   extern float timefactor;
   // D term 1 weighted + P term 1 weighted
-  apidoutput1[x] += (state.angleerror[x] - lasterror[x]) * profile.pid.small_angle.kd * (1 - fabsf(state.angleerror[x])) * timefactor;
+  apidoutput1[x] += (state.angleerror[x] - lasterror[x]) * profile.pid.small_angle.kd * inverse_angle_error * timefactor;
 
   // D term 2 weighted + P term 2 weighted
-  apidoutput2[x] += ((state.angleerror[x] - lasterror[x]) * profile.pid.big_angle.kd * fabsf(state.angleerror[x]) * timefactor);
+  apidoutput2[x] += ((state.angleerror[x] - lasterror[x]) * profile.pid.big_angle.kd * abs_angle_error * timefactor);
 
   // apidoutput sum
   apidoutput[x] = apidoutput1[x] + apidoutput2[x];
