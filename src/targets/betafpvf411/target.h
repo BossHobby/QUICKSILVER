Quick-Flash/NFE_QUICKSILVER

add betafpvf411 target

src/targets/betafpvf411/target.h
@@ -1,0 +1,90 @@
+#include "config.h"
+#include "config_helper.h"
+
+#define BetaFPVF411
+
+#define F4
+#define F411
+
+//PORTS
+#define SPI_PORTS   \
+  SPI1_PA5PA6PA7    \
+  SPI2_PB13PB14PB15 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2
+
+//#define USB_DETECT_PIN GPIO_Pin_15
+//#define USB_DETECT_PORT GPIOC
+
+//LEDS
+#define LED_NUMBER 2
+#define LED1PIN PIN_C13
+#define LED2PIN PIN_C14
+
+#define BUZZER_PIN PIN_B2
+
+//#define FPV_PIN GPIO_Pin_13
+//#define FPV_PORT GPIOA
+
+//GYRO
+#define MPU6XXX_SPI_PORT SPI_PORT1
+#define MPU6XXX_NSS PIN_A4
+#define MPU6XXX_INT PIN_A1
+#define USE_DUMMY_I2C
+#define SENSOR_ROTATE_90_CCW
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x73
+#define GYRO_ID_3 0x78
+#define GYRO_ID_4 0x71
+
+//RADIO
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#define SOFTSPI_NONE
+#endif
+
+#ifndef SOFTSPI_NONE
+#define RADIO_CHECK
+#define SPI_MISO_PIN GPIO_Pin_10
+#define SPI_MISO_PORT GPIOA
+#define SPI_MOSI_PIN GPIO_Pin_9
+#define SPI_MOSI_PORT GPIOA
+#define SPI_CLK_PIN GPIO_Pin_3
+#define SPI_CLK_PORT GPIOA
+#define SPI_SS_PIN GPIO_Pin_2
+#define SPI_SS_PORT GPIOA
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+#define USE_M25P16
+#define M25P16_SPI_PORT SPI_PORT2
+#define M25P16_NSS_PIN PIN_A0
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN PIN_B0
+#define BATTERY_ADC_CHANNEL ADC_Channel_8
+
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+#define MOTOR_PIN0 MOTOR_PIN_PB6
+#define MOTOR_PIN1 MOTOR_PIN_PB7
+#define MOTOR_PIN2 MOTOR_PIN_PB4
+#define MOTOR_PIN3 MOTOR_PIN_PB5

BossHobby/QUICKSILVER

src/targets/betafpvf411/target.h
@@ -1,0 +1,87 @@
+#include "config.h"
+#include "config_helper.h"
+
+#define BetaFPVF411
+
+//PORTS
+#define SPI_PORTS   \
+  SPI1_PA5PA6PA7    \
+  SPI2_PB13PB14PB15 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2
+
+//#define USB_DETECT_PIN GPIO_Pin_15
+//#define USB_DETECT_PORT GPIOC
+
+//LEDS
+#define LED_NUMBER 2
+#define LED1PIN PIN_C13
+#define LED2PIN PIN_C14
+
+#define BUZZER_PIN PIN_B2
+
+//#define FPV_PIN GPIO_Pin_13
+//#define FPV_PORT GPIOA
+
+//GYRO
+#define GYRO_TYPE MPU6XXX
+#define GYRO_SPI_PORT SPI_PORT1
+#define GYRO_NSS PIN_A4
+#define GYRO_INT PIN_A1
+#define SENSOR_ROTATE_90_CCW
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x73
+#define GYRO_ID_3 0x78
+#define GYRO_ID_4 0x71
+
+//RADIO
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#define SOFTSPI_NONE
+#endif
+
+#ifndef SOFTSPI_NONE
+#define RADIO_CHECK
+#define SPI_MISO_PIN GPIO_Pin_10
+#define SPI_MISO_PORT GPIOA
+#define SPI_MOSI_PIN GPIO_Pin_9
+#define SPI_MOSI_PORT GPIOA
+#define SPI_CLK_PIN GPIO_Pin_3
+#define SPI_CLK_PORT GPIOA
+#define SPI_SS_PIN GPIO_Pin_2
+#define SPI_SS_PORT GPIOA
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+#define USE_M25P16
+#define M25P16_SPI_PORT SPI_PORT2
+#define M25P16_NSS_PIN PIN_A0
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN PIN_B0
+#define BATTERY_ADC_CHANNEL LL_ADC_CHANNEL_8
+
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+#define MOTOR_PIN0 MOTOR_PIN_PB6
+#define MOTOR_PIN1 MOTOR_PIN_PB7
+#define MOTOR_PIN2 MOTOR_PIN_PB4
+#define MOTOR_PIN3 MOTOR_PIN_PB5