83311d149233ac9f8c193c46b659a4d8babec963
bkleiner <b.kleiner@str8labs.com>
2021-12-13 00:17:23 +0100

    interrupt: reorder prios

--- src/drivers/drv_interrupt.h
+++ src/drivers/drv_interrupt.h
@@ -3,8 +3,8 @@
 #include "project.h"
 
 #define UART_PRIORITY 0x0, 0x0
-#define TIMER_PRIORITY 0x1, 0x0
+#define TIMER_PRIORITY 0x0, 0x1
+#define DMA_PRIORITY 0x1, 0x0
 #define EXTI_PRIORITY 0x1, 0x1
-#define DMA_PRIORITY 0x2, 0x2
 
 void interrupt_enable(IRQn_Type irq, uint32_t preempt_priority, uint32_t sub_priority);
