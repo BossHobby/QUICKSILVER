cf20b80c8ad4607f5c8e5d4434751b16218b984c
bkleiner <b.kleiner@str8labs.com>
2022-01-23 12:14:23 +0100

    add furyf4osd

---
+++ src/targets/furyf4osd/target.h
@@ -0,0 +1,75 @@
+
+#include "config.h"
+#include "config_helper.h"
+
+#define FURYF4OSD
+
+// PORTS
+#define SPI_PORTS   \
+  SPI1_PA5PA6PA7    \
+  SPI2_PB13PB14PB15 \
+  SPI3_PC10PC11PC12
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART3_PB11PB10   \
+  USART6_PC7PC6
+
+// LEDS
+#define LED_NUMBER 2
+#define LED1PIN PIN_B5
+#define LED1_INVERT
+#define LED2PIN PIN_B4
+#define LED2_INVERT
+
+#define BUZZER_PIN PIN_A8
+#define BUZZER_INVERT
+
+// GYRO
+#define GYRO_TYPE MPU6XXX
+#define GYRO_SPI_PORT SPI_PORT1
+#define GYRO_NSS PIN_A4
+#define GYRO_INT PIN_C4
+#define SENSOR_ROTATE_90_CCW
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x73
+#define GYRO_ID_3 0x78
+#define GYRO_ID_4 0x71
+
+// RADIO
+#define USART1_INVERTER_PIN PIN_C0
+
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT1
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+// VOLTAGE DIVIDER
+#define BATTERYPIN PIN_C1
+#define BATTERY_ADC_CHANNEL LL_ADC_CHANNEL_11
+
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+// S3_OUT
+#define MOTOR_PIN0 MOTOR_PIN_PB1
+// S4_OUT
+#define MOTOR_PIN1 MOTOR_PIN_PA2
+// S1_OUT
+#define MOTOR_PIN2 MOTOR_PIN_PA3
+// S2_OUT
+#define MOTOR_PIN3 MOTOR_PIN_PB0
