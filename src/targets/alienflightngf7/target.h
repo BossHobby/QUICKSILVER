f95319e48330ed615cf2e3fa8394d6d3c01feeef
bkleiner <b.kleiner@str8labs.com>
2022-01-30 14:21:52 +0100

    add alienflightngf7

---
+++ src/targets/alienflightngf7/target.h
@@ -0,0 +1,72 @@
+
+#include "config.h"
+#include "config_helper.h"
+
+#define ALIENFLIGHTNGF7
+
+//PORTS
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7  \
+  SPI2_PB13PC2PC3 \
+  SPI3_PB3PB4PB5
+
+#define USART_PORTS \
+  USART1_PA10PA9    \
+  USART2_PA3PA2     \
+  USART4_PC11PC10
+
+//LEDS
+#define LED_NUMBER 2
+#define LED1PIN PIN_C12
+#define LED1_INVERT
+#define LED2PIN PIN_D2
+#define LED2_INVERT
+
+#define BUZZER_PIN PIN_C13
+
+//GYRO
+#define GYRO_TYPE MPU6XXX
+#define GYRO_SPI_PORT SPI_PORT1
+#define GYRO_NSS PIN_A4
+#define GYRO_INT PIN_C14
+#define SENSOR_ROTATE_90_CCW
+#define GYRO_ID_1 0x68
+#define GYRO_ID_2 0x73
+#define GYRO_ID_3 0x78
+#define GYRO_ID_4 0x71
+
+//RADIO
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT3
+#define MAX7456_NSS PIN_B12
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN PIN_C0
+#define BATTERY_ADC_CHANNEL LL_ADC_CHANNEL_10
+
+#ifndef VOLTAGE_DIVIDER_R1
+#define VOLTAGE_DIVIDER_R1 10000
+#endif
+
+#ifndef VOLTAGE_DIVIDER_R2
+#define VOLTAGE_DIVIDER_R2 1000
+#endif
+
+#ifndef ADC_REF_VOLTAGE
+#define ADC_REF_VOLTAGE 3.3
+#endif
+
+// MOTOR PINS
+//S3_OUT
+#define MOTOR_PIN0 MOTOR_PIN_PB14
+//S4_OUT
+#define MOTOR_PIN1 MOTOR_PIN_PB0
+//S1_OUT
+#define MOTOR_PIN2 MOTOR_PIN_PC6
+//S2_OUT
+#define MOTOR_PIN3 MOTOR_PIN_PC7
