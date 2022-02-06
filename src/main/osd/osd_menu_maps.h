4e8bb054d791fa2e1c85e23285a51aa245f76347
bkleiner <b.kleiner@str8labs.com>
2022-02-06 15:55:12 +0100

    osd: format

--- src/main/osd/osd_menu_maps.h
+++ src/main/osd/osd_menu_maps.h
@@ -1,16 +1,16 @@
 #include "stdio.h"
 
-//main menu maps
+// main menu maps
 extern const char main_menu_labels[10][21];
 extern const uint8_t main_menu_positions[10][2];
 extern const uint8_t main_menu_map[];
 
-//pid profiles submenu map
+// pid profiles submenu map
 extern const char pid_profiles_labels[3][21];
 extern const uint8_t pid_profiles_positions[3][2];
 extern const uint8_t pid_submenu_map[];
 
-//adjust increments used in vector adjust functions
+// adjust increments used in vector adjust functions
 extern const float bf_pids_increments[];
 extern const float sw_rates_increments[];
 extern const float rounded_increments[];
@@ -24,17 +24,17 @@ extern const uint8_t pid_profile_grid[9][2];
 extern const uint8_t pid_profile_data_positions[9][2];
 extern const float pid_profile_adjust_limits[9][2];
 
-//filters submenu map
+// filters submenu map
 extern const char filter_labels[3][21];
 extern const uint8_t filter_positions[3][2];
 extern const uint8_t filter_submenu_map[2];
 
-//rates submenu map
+// rates submenu map
 extern const char rates_profile_labels[3][21];
 extern const uint8_t rates_profile_positions[3][2];
 extern const uint8_t rates_submenu_map[];
 
-//silverware rates map
+// silverware rates map
 extern const char sw_rates_labels[8][21];
 extern const uint8_t sw_rates_positions[8][2];
 extern const uint8_t sw_rates_data_index[9][2];
@@ -42,7 +42,7 @@ extern const uint8_t sw_rates_grid[9][2];
 extern const uint8_t sw_rates_data_positions[9][2];
 extern const float sw_rates_adjust_limits[9][2];
 
-//betaflight rates map
+// betaflight rates map
 extern const char bf_rates_labels[8][21];
 extern const uint8_t bf_rates_positions[8][2];
 extern const uint8_t bf_rates_data_index[9][2];
@@ -50,7 +50,7 @@ extern const uint8_t bf_rates_grid[9][2];
 extern const uint8_t bf_rates_data_positions[9][2];
 extern const float bf_rates_adjust_limits[9][2];
 
-//flight modes map
+// flight modes map
 extern uint8_t *flight_modes_ptr[10];
 extern const char flight_modes_labels[12][21];
 extern const uint8_t flight_modes_positions[12][2];
@@ -59,19 +59,19 @@ extern const uint8_t flight_modes_aux_limits[];
 extern const uint8_t flight_modes_aux_items[];
 extern const uint8_t flight_modes_grid[10][2];
 
-//osd elements submenu map
+// osd elements submenu map
 extern const char osd_elements_menu_labels[5][21];
 extern const uint8_t osd_elements_menu_positions[5][2];
 extern const uint8_t osd_elements_map[];
 
-//osd element add/remove & text/invert submenu map
+// osd element add/remove & text/invert submenu map
 extern const char osd_display_labels[12][21];
 extern const uint8_t osd_display_positions[12][2];
 extern const uint8_t osd_display_data_positions[10][2];
 extern const uint8_t osd_display_grid[10][2];
 extern const uint8_t osd_elements_active_items[];
 
-//osd positions submenu map
+// osd positions submenu map
 extern const char osd_position_labels[14][21];
 extern const uint8_t osd_position_adjust_positions[14][2];
 extern const uint8_t osd_position_grid[20][2];
@@ -79,21 +79,21 @@ extern const uint8_t osd_position_data_positions[20][2];
 extern const uint8_t osd_position_active_items[];
 extern const uint8_t osd_position_index[20];
 
-//osd text style submenu map
+// osd text style submenu map
 extern const char osd_text_style[12][21];
 extern const uint8_t osd_text_style_positions[12][2];
 
-//osd callsign edit submenu map
+// osd callsign edit submenu map
 extern const char osd_callsign_edit_labels[23][21];
 extern const uint8_t osd_callsign_edit_positions[23][2];
 extern const uint8_t osd_callsign_grid[20][2];
 extern const uint8_t osd_callsign_edit_data_positions[20][2];
 extern const uint8_t callsign_shift_index[20][2];
 
-//vtx submenu map
+// vtx submenu map
 extern const char vtx_na_labels[3][21];
 extern const uint8_t vtx_na_positions[3][2];
-//vtx menu map
+// vtx menu map
 extern uint8_t *vtx_ptr[4];
 extern const char vtx_labels[6][21];
 extern const uint8_t vtx_positions[6][2];
@@ -101,17 +101,17 @@ extern const uint8_t vtx_data_positions[4][2];
 extern const uint8_t vtx_limits[4];
 extern const uint8_t vtx_grid[4][2];
 
-//special features menu map
+// special features menu map
 extern const char special_features_labels[8][21];
 extern const uint8_t special_features_positions[8][2];
 extern const uint8_t special_features_map[];
 
-//stick boost submenu map
+// stick boost submenu map
 extern const char stickboost_labels[3][21];
 extern const uint8_t stickboost_profile_positions[3][2];
 extern const uint8_t stickboost_submenu_map[];
 
-//stick boost map
+// stick boost map
 extern const char stickboost1_labels[7][21];
 extern const char stickboost2_labels[7][21];
 extern const uint8_t stickboost_positions[7][2];
@@ -120,7 +120,7 @@ extern const uint8_t stickboost_grid[6][2];
 extern const uint8_t stickboost_data_positions[9][2];
 extern const float stickboost_adjust_limits[6][2];
 
-//low battery map
+// low battery map
 extern float *low_batt_ptr[1];
 extern const char lowbatt_labels[3][21];
 extern const uint8_t lowbatt_positions[3][2];
@@ -128,12 +128,12 @@ extern const uint8_t lowbatt_grid[1][2];
 extern const uint8_t lowbatt_data_positions[1][2];
 extern const float lowbatt_adjust_limits[1][2];
 
-//levelmode submenu map
+// levelmode submenu map
 extern const char level_submenu_labels[3][21];
 extern const uint8_t level_submenu_positions[3][2];
 extern const uint8_t level_submenu_map[];
 
-//levelmode maxangle map
+// levelmode maxangle map
 extern float *level_maxangle_ptr[1];
 extern const char maxangle_labels[3][21];
 extern const uint8_t maxangle_positions[3][2];
@@ -141,7 +141,7 @@ extern const uint8_t maxangle_grid[6][2];
 extern const uint8_t maxangle_data_positions[1][2];
 extern const float maxangle_adjust_limits[1][2];
 
-//levelmode pid map
+// levelmode pid map
 extern float *level_pid_ptr[4];
 extern const char levelmode_labels[6][21];
 extern const uint8_t levelmode_positions[6][2];
@@ -149,12 +149,12 @@ extern const uint8_t levelmode_grid[4][2];
 extern const uint8_t levelmode_data_positions[4][2];
 extern const float levelmode_adjust_limits[4][2];
 
-//motor boost menu map
+// motor boost menu map
 extern const char motor_boost_labels[3][21];
 extern const uint8_t motor_boost_positions[3][2];
 extern const uint8_t motor_boost_map[];
 
-//torque boost map
+// torque boost map
 extern float *torqueboost_ptr[1];
 extern const char torqueboost_labels[3][21];
 extern const uint8_t torqueboost_positions[3][2];
@@ -162,7 +162,7 @@ extern const uint8_t torqueboost_grid[1][2];
 extern const uint8_t torqueboost_data_positions[1][2];
 extern const float torqueboost_adjust_limits[1][2];
 
-//throttle boost map
+// throttle boost map
 extern float *throttleboost_ptr[1];
 extern const char throttleboost_labels[3][21];
 extern const uint8_t throttleboost_positions[3][2];
@@ -170,7 +170,7 @@ extern const uint8_t throttleboost_grid[1][2];
 extern const uint8_t throttleboost_data_positions[1][2];
 extern const float throttleboost_adjust_limits[1][2];
 
-//digital idle map
+// digital idle map
 extern float *motoridle_ptr[1];
 extern const char motoridle_labels[3][21];
 extern const uint8_t motoridle_positions[3][2];
@@ -178,7 +178,7 @@ extern const uint8_t motoridle_grid[1][2];
 extern const uint8_t motoridle_data_positions[1][2];
 extern const float motoridle_adjust_limits[1][2];
 
-//turtle map
+// turtle map
 extern float *turtlethrottle_ptr[1];
 extern const char turtlethrottle_labels[3][21];
 extern const uint8_t turtlethrottle_positions[3][2];
@@ -186,9 +186,9 @@ extern const uint8_t turtlethrottle_grid[1][2];
 extern const uint8_t turtlethrottle_data_positions[1][2];
 extern const float turtlethrottle_adjust_limits[1][2];
 
-//gyro filter map
+// gyro filter map
 #define POINTER_REDIRECT -999.0f
-//static float pointer_redirect;
+// static float pointer_redirect;
 extern float *gyrofilter_ptr[4];
 extern uint8_t *gyrofilter_ptr2[4];
 extern const uint8_t gyrofilter_reboot_request[4];
@@ -199,7 +199,7 @@ extern const uint8_t gyrofilter_grid[4][2];
 extern const uint8_t gyrofilter_data_positions[4][2];
 extern const float gyrofilter_adjust_limits[4][2];
 
-//dterm filter map
+// dterm filter map
 extern float *dtermfilter_ptr[7];
 extern uint8_t *dtermfilter_ptr2[7];
 extern const uint8_t dtermfilter_reboot_request[7];
@@ -210,7 +210,7 @@ extern const uint8_t dtermfilter_grid[7][2];
 extern const uint8_t dtermfilter_data_positions[7][2];
 extern const float dtermfilter_adjust_limits[7][2];
 
-//pid modifiers map
+// pid modifiers map
 extern float *pidmodify_ptr[4];
 extern uint8_t *pidmodify_ptr2[4];
 extern const uint8_t pidmodify_reboot_request[4];
@@ -221,12 +221,12 @@ extern const uint8_t pidmodify_grid[4][2];
 extern const uint8_t pidmodify_data_positions[4][2];
 extern const float pidmodify_adjust_limits[4][2];
 
-//rc link map
+// rc link map
 extern const char rc_link_labels[3][21];
 extern const uint8_t rc_link_positions[3][2];
 extern const uint8_t rc_link_map[];
 
-//rssi source map
+// rssi source map
 extern uint8_t *rssi_source_ptr[2];
 extern const char rssi_menu_labels[4][21];
 extern const uint8_t rssi_menu_positions[4][2];
@@ -234,22 +234,22 @@ extern const uint8_t rssi_source_limits[];
 extern const uint8_t rssi_source_data_positions[2][2];
 extern const uint8_t rssi_source_data_grid[2][2];
 
-//stick wizard map 1
+// stick wizard map 1
 extern const char stick_wizard_labels_1[3][21];
 extern const uint8_t stick_wizard_positions_1[3][2];
 
-//stick wizard map 2
+// stick wizard map 2
 extern const char stick_wizard_labels_2[3][21];
 extern const uint8_t stick_wizard_positions_2[3][2];
 
-//stick wizard map 3
+// stick wizard map 3
 extern const char stick_wizard_labels_3[3][21];
 extern const uint8_t stick_wizard_positions_3[3][2];
 
-//stick wizard map 4
+// stick wizard map 4
 extern const char stick_wizard_labels_4[4][21];
 extern const uint8_t stick_wizard_positions_4[4][2];
 
-//stick wizard map 5
+// stick wizard map 5
 extern const char stick_wizard_labels_5[4][21];
 extern const uint8_t stick_wizard_positions_5[4][2];
