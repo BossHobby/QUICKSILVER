bd4aa2bdee9e6e80ba5e673f07a86d0426976ce0
bkleiner <b.kleiner@str8labs.com>
2022-02-12 21:23:24 +0100

    add simple osd transaction api, use for regular osd display

---
+++ src/drivers/drv_osd.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define OSD_TXN_BUFFER 128
+#define OSD_TXN_MAX 16
+
+typedef struct {
+  uint8_t attr;
+
+  uint8_t x;
+  uint8_t y;
+
+  uint8_t offset;
+  uint8_t size;
+} osd_segment_t;
+
+typedef struct {
+  uint8_t buffer[OSD_TXN_BUFFER];
+
+  osd_segment_t segments[OSD_TXN_MAX];
+  uint8_t segment_count;
+} osd_transaction_t;
+
+bool osd_is_ready();
+
+osd_transaction_t *osd_txn_init();
+
+void osd_txn_start(uint8_t attr, uint8_t x, uint8_t y);
+
+void osd_txn_write_data(const uint8_t *buffer, uint8_t size);
+void osd_txn_write_str(const char *buffer);
+void osd_txn_write_char(const char val);
+void osd_txn_write_uint(uint32_t val, uint8_t width);
+void osd_txn_write_int(int32_t val, uint8_t width);
+void osd_txn_write_float(float val, uint8_t width, uint8_t precision);
+
+void osd_txn_submit(osd_transaction_t *txn);
