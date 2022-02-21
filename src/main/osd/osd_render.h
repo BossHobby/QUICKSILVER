12aebf484ba4aeacdd9a3784e8d041a7cec6f7b8
bkleiner <b.kleiner@str8labs.com>
2022-02-21 02:09:25 +0100

    widen osd element positions for hd

--- src/main/osd/osd_render.h
+++ src/main/osd/osd_render.h
@@ -18,6 +18,33 @@ typedef enum {
   OSD_ELEMENT_MAX
 } osd_elements_t;
 
+/*
+screen elements characteristics written like registers in a 32bit binany number
+BIT
+0			-		0 is display element inactive , 1 is display element active
+1			-		0 is TEXT, 1 is INVERT
+2:10	-		the X screen position (column)
+11:17	-		the Y screen position	(row)
+17:32	-		not currently used
+*/
+
+typedef enum {
+  ACTIVE = 0,
+  ATTRIBUTE = 1,
+  POSITIONX = 2,
+  POSITIONY = 3,
+} osd_element_attrs_t;
+
+typedef struct {
+  uint32_t active : 1;
+  uint32_t attribute : 1;
+  uint32_t pos_x : 8;
+  uint32_t pos_y : 8;
+  uint32_t _unused : 14;
+} __attribute__((packed)) osd_element_t;
+
+#define ENCODE_OSD_ELEMENT(active, attr, x, y) ((y << 10) | (x << 2) | (attr << 1) | active)
+
 void osd_init();
 void osd_display();
 void osd_clear();
