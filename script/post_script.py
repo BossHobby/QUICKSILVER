98b6dbfce35a42ae9f722b01d74bc07036dfd8d2
bkleiner <b.kleiner@str8labs.com>
2022-04-14 23:01:32 +0200

    objcopy: quote paths

--- script/post_script.py
+++ script/post_script.py
@@ -19,7 +19,7 @@ env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.elf",
     env.VerboseAction(" ".join([
         "$OBJCOPY", "-O", "ihex",
-        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
+        "\"$BUILD_DIR/${PROGNAME}.elf\"", "\"$BUILD_DIR/${PROGNAME}.hex\""
     ]), "Building $BUILD_DIR/${PROGNAME}.hex")
 )
 
