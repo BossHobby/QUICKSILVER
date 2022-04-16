df3d1bdab73ca55a04c12bcef966c09cac32a0af
bkleiner <b.kleiner@str8labs.com>
2022-04-16 17:32:09 +0200

    enable lto across the codebase

--- src/system/stm32f745/system.c
+++ src/system/stm32f745/system.c
@@ -258,7 +258,7 @@ void SystemClock_Config(void) {
   * @param  None
   * @retval None
   */
-void SystemInit(void) {
+__attribute__((__used__)) void SystemInit() {
 /* FPU settings ------------------------------------------------------------*/
 #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
   SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); /* set CP10 and CP11 Full Access */
