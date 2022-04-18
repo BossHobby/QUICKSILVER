b5f80b606601dc22c408e809ac3ae4a8bb80f720
bkleiner <b.kleiner@str8labs.com>
2022-04-18 04:51:29 +0200

    add more generic msp implementation, use for usb

---
+++ src/io/msp.c
@@ -0,0 +1,221 @@
+#include "msp.h"
+
+#include <stdbool.h>
+#include <string.h>
+
+#include "drv_serial_4way.h"
+#include "flight/control.h"
+#include "util/util.h"
+
+static uint32_t msp_available(msp_t *msp) {
+  if (msp->write_offset >= msp->read_offset) {
+    return msp->write_offset - msp->read_offset;
+  }
+  return msp->buffer_size + msp->write_offset - msp->read_offset;
+}
+
+static bool msp_get(msp_t *msp, uint32_t offset, uint8_t *data) {
+  if (offset >= msp_available(msp)) {
+    return false;
+  }
+  *data = msp->buffer[msp->read_offset + offset];
+  return true;
+}
+
+static bool msp_expect(msp_t *msp, uint32_t offset, uint8_t val) {
+  uint8_t data = 0;
+  if (!msp_get(msp, offset, &data)) {
+    return false;
+  }
+
+  return data == val;
+}
+
+static void msp_send_reply(msp_t *msp, uint8_t code, uint8_t *data, uint8_t len) {
+  if (msp->send) {
+    msp->send('>', code, data, len);
+  }
+}
+
+static void msp_send_error(msp_t *msp, uint8_t code) {
+  if (msp->send) {
+    msp->send('!', code, NULL, 0);
+  }
+}
+
+void msp_push_byte(msp_t *msp, uint8_t val) {
+  msp->buffer[msp->write_offset] = val;
+  msp->write_offset++;
+}
+
+void msp_push(msp_t *msp, uint8_t *data, uint32_t size) {
+  memcpy(msp->buffer, data, size);
+  msp->write_offset += size;
+}
+
+static void msp_process_serial_cmd(msp_t *msp, uint8_t cmd, uint8_t *payload, uint8_t size) {
+  switch (cmd) {
+  case MSP_API_VERSION: {
+    uint8_t data[3] = {
+        0,  // MSP_PROTOCOL_VERSION
+        1,  // API_VERSION_MAJOR
+        41, // API_VERSION_MINOR
+    };
+    msp_send_reply(msp, cmd, data, 3);
+    break;
+  }
+  case MSP_FC_VARIANT: {
+    uint8_t data[4] = {'Q', 'U', 'I', 'C'};
+    msp_send_reply(msp, cmd, data, 4);
+    break;
+  }
+  case MSP_FC_VERSION: {
+    uint8_t data[3] = {
+        0, // FC_VERSION_MAJOR
+        1, // FC_VERSION_MINOR
+        0, // FC_VERSION_PATCH_LEVEL
+    };
+    msp_send_reply(msp, cmd, data, 3);
+    break;
+  }
+  case MSP_BUILD_INFO: {
+    uint8_t data[19] = MSP_BUILD_DATE_TIME;
+    msp_send_reply(msp, cmd, data, 19);
+    break;
+  }
+  case MSP_BOARD_INFO: {
+    uint8_t data[6] = {'Q', 'U', 'I', 'C', 0, 0};
+    msp_send_reply(msp, cmd, data, 6);
+    break;
+  }
+  case MSP_UID: {
+    uint8_t data[12] = {
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+
+        0xd,
+        0xe,
+        0xa,
+        0xd,
+
+        0xb,
+        0xe,
+        0xe,
+        0xf,
+    };
+    msp_send_reply(msp, cmd, data, 12);
+    break;
+  }
+  case MSP_BATTERY_STATE: {
+    const uint16_t current = state.ibat / 1000;
+    uint8_t data[9] = {
+        state.lipo_cell_count,                                  // battery detected
+        0x0, 0x0,                                               // battery capacity
+        (uint8_t)constrainf(state.vbat_filtered / 0.1, 0, 255), // battery voltage
+        0x0, 0x0,                                               // battery drawn in mAh
+        (current >> 8) & 0xFF, current & 0xFF,                  // battery current draw in A
+        0x0                                                     // battery status
+    };
+    msp_send_reply(msp, cmd, data, 9);
+    break;
+  }
+  case MSP_FEATURE_CONFIG: {
+    uint8_t data[4] = {
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+    };
+    msp_send_reply(msp, cmd, data, 4);
+    break;
+  }
+  case MSP_MOTOR_CONFIG: {
+    uint16_t data[3] = {
+        1070, // min throttle
+        2000, // max throttle
+        1000, // min command
+    };
+    msp_send_reply(msp, cmd, (uint8_t *)data, 3 * sizeof(uint16_t));
+    break;
+  }
+  case MSP_MOTOR: {
+    // we always have 4 motors, but blheli expects 8
+    // these are pwm values
+    uint16_t data[8] = {
+        (uint16_t)mapf(motor_test.value[0], 0.0f, 1.0f, 1000.f, 2000.f),
+        (uint16_t)mapf(motor_test.value[1], 0.0f, 1.0f, 1000.f, 2000.f),
+        (uint16_t)mapf(motor_test.value[2], 0.0f, 1.0f, 1000.f, 2000.f),
+        (uint16_t)mapf(motor_test.value[3], 0.0f, 1.0f, 1000.f, 2000.f),
+        0,
+        0,
+        0,
+        0,
+    };
+    msp_send_reply(msp, cmd, (uint8_t *)data, 8 * sizeof(uint16_t));
+    break;
+  }
+  case MSP_SET_MOTOR: {
+    uint16_t *values = (uint16_t *)(payload);
+    for (uint8_t i = 0; i < 4; i++) {
+      motor_test.value[i] = mapf(values[i], 1000.f, 2000.f, 0.0f, 1.0f);
+    }
+    motor_test.active = 1;
+
+    msp_send_reply(msp, cmd, NULL, 0);
+    break;
+  }
+#ifdef USE_SERIAL_4WAY_BLHELI_INTERFACE
+  case MSP_SET_4WAY_IF: {
+    uint8_t data[1] = {4};
+    msp_send_reply(msp, cmd, data, 1);
+
+    motor_test.active = 0;
+
+    serial_4way_init();
+    serial_4way_process();
+
+    break;
+  }
+#endif
+  default:
+    msp_send_error(msp, cmd);
+    break;
+  }
+}
+
+msp_status_t msp_process_serial(msp_t *msp) {
+  if (msp_available(msp) < MSP_HEADER_LEN) {
+    return MSP_EOF;
+  }
+
+  if (!msp_expect(msp, 0, '$') || !msp_expect(msp, 1, 'M') || !msp_expect(msp, 2, '<')) {
+    msp->read_offset++;
+    return MSP_ERROR;
+  }
+
+  uint8_t size = 0;
+  uint8_t cmd = 0;
+  if (!msp_get(msp, 3, &size) || !msp_get(msp, 4, &cmd)) {
+    return MSP_ERROR;
+  }
+
+  if (msp_available(msp) < (MSP_HEADER_LEN + size + 1)) {
+    return MSP_EOF;
+  }
+
+  uint8_t chksum = size ^ cmd;
+  for (uint8_t i = 0; i < size; i++) {
+    chksum ^= msp->buffer[MSP_HEADER_LEN + i];
+  }
+
+  if (!msp_expect(msp, MSP_HEADER_LEN + size, chksum)) {
+    msp->read_offset += (MSP_HEADER_LEN + size + 1);
+    return MSP_ERROR;
+  }
+
+  msp_process_serial_cmd(msp, cmd, msp->buffer + MSP_HEADER_LEN, size);
+  msp->read_offset += (MSP_HEADER_LEN + size + 1);
+  return MSP_SUCCESS;
+}
--- src/usb/usb_configurator.c
+++ src/usb/usb_configurator.c
@@ -3,6 +3,7 @@
 #include "debug.h"
 #include "drv_usb.h"
 #include "flight/control.h"
+#include "io/msp.h"
 #include "profile.h"
 #include "project.h"
 #include "reset.h"
@@ -12,11 +13,45 @@ extern profile_t profile;
 uint8_t encode_buffer[USB_BUFFER_SIZE];
 uint8_t decode_buffer[USB_BUFFER_SIZE];
 
+void usb_msp_send(uint8_t direction, uint8_t code, uint8_t *data, uint8_t len);
+
+static msp_t msp = {
+    .buffer = decode_buffer,
+    .buffer_size = USB_BUFFER_SIZE,
+    .read_offset = 0,
+    .write_offset = 0,
+    .send = usb_msp_send,
+};
+
+void usb_msp_send(uint8_t direction, uint8_t code, uint8_t *data, uint8_t len) {
+  const uint8_t size = len + MSP_HEADER_LEN + 1;
+
+  static uint8_t frame[64];
+
+  frame[0] = '$';
+  frame[1] = 'M';
+  frame[2] = '>';
+  frame[3] = len;
+  frame[4] = code;
+
+  for (uint8_t i = 0; i < len; i++) {
+    frame[i + MSP_HEADER_LEN] = data[i];
+  }
+
+  uint8_t chksum = len;
+  for (uint8_t i = 4; i < (size - 1); i++) {
+    chksum ^= frame[i];
+  }
+  frame[len + MSP_HEADER_LEN] = chksum;
+
+  usb_serial_write(frame, size);
+}
+
 // double promition in the following is intended
 #pragma GCC diagnostic ignored "-Wdouble-promotion"
 // This function will be where all usb send/receive coms live
 void usb_configurator() {
-  static uint8_t magic = 0;
+  uint8_t magic = 0;
   if (usb_serial_read(&magic, 1) != 1) {
     return;
   }
@@ -29,9 +64,19 @@ void usb_configurator() {
   case USB_MAGIC_SOFT_REBOOT:
     system_reset();
     break;
-  case USB_MAGIC_MSP:
-    usb_process_msp();
+  case USB_MAGIC_MSP: {
+    msp_push_byte(&msp, magic);
+
+    while (true) {
+      msp_status_t status = msp_process_serial(&msp);
+      if (status == MSP_EOF) {
+        msp_push_byte(&msp, usb_serial_read_byte());
+      } else {
+        break;
+      }
+    }
     break;
+  }
   case USB_MAGIC_QUIC:
     usb_process_quic();
     break;
