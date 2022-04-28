6f659a05ceb64007261ccff273b2a3fe181cd5c5
bkleiner <b.kleiner@str8labs.com>
2022-04-28 22:56:31 +0200

    h7: split ram, enable dma cache protection

--- src/drivers/drv_dma.c
+++ src/drivers/drv_dma.c
@@ -2,13 +2,13 @@
 
 #include "project.h"
 
-#ifdef STM32F7
+#if defined(STM32F7) || defined(STM32H7)
 #define CACHE_LINE_SIZE 32
 #define CACHE_LINE_MASK (CACHE_LINE_SIZE - 1)
 #endif
 
 void dma_prepare_tx_memory(uint8_t *addr, uint32_t size) {
-#ifdef STM32F7
+#if defined(STM32F7) || defined(STM32H7)
   if (!WITHIN_DTCM_RAM(addr)) {
     SCB_CleanDCache_by_Addr(
         (uint32_t *)((uint32_t)addr & ~CACHE_LINE_MASK),
@@ -18,7 +18,7 @@ void dma_prepare_tx_memory(uint8_t *addr, uint32_t size) {
 }
 
 void dma_prepare_rx_memory(uint8_t *addr, uint32_t size) {
-#ifdef STM32F7
+#if defined(STM32F7) || defined(STM32H7)
   if (!WITHIN_DTCM_RAM(addr)) {
     SCB_CleanInvalidateDCache_by_Addr(
         (uint32_t *)((uint32_t)addr & ~CACHE_LINE_MASK),
