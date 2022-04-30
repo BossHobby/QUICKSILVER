a8603b151318182b7006e621878dbbda63eb659e
bkleiner <b.kleiner@str8labs.com>
2022-04-30 17:26:10 +0200

    flash: rework to allow for different alignment

--- src/drivers/drv_fmc.h
+++ src/drivers/drv_fmc.h
@@ -2,13 +2,23 @@
 
 #include <stdint.h>
 
+#if defined(STM32H7)
+#define FLASH_WORD_SIZE 32
+typedef uint64_t flash_word_t;
+#else
+#define FLASH_WORD_SIZE 4
+typedef uint32_t flash_word_t;
+#endif
+
+#define FLASH_ALIGN(offset) ((offset + (FLASH_WORD_SIZE - 1)) & -FLASH_WORD_SIZE)
+
 void fmc_lock();
 void fmc_unlock();
 
 uint8_t fmc_erase();
 
-uint32_t fmc_read(uint32_t addr);
-float fmc_read_float(uint32_t addr);
+flash_word_t fmc_read(uint32_t addr);
+void fmc_read_buf(uint32_t offset, uint8_t *data, uint32_t size);
 
-void fmc_write(uint32_t addr, uint32_t value);
-void fmc_write_float(uint32_t addr, float value);
+void fmc_write(uint32_t addr, flash_word_t value);
+void fmc_write_buf(uint32_t addr, uint8_t *data, uint32_t size);
--- src/flash.h
+++ src/flash.h
@@ -6,21 +6,21 @@
 #include "rx_frsky.h"
 #include "rx_unified_serial.h"
 
-#define FLASH_STORAGE_OFFSET 4
-#define FLASH_STORAGE_SIZE 32
+#define FLASH_STORAGE_OFFSET FLASH_ALIGN(4)
+#define FLASH_STORAGE_SIZE FLASH_ALIGN(32)
 
 typedef struct {
   float pid_identifier;
   float accelcal[3];
 
-  uint8_t flash_feature_1; //SETUP WIZARD
+  uint8_t flash_feature_1; // SETUP WIZARD
   uint8_t lvc_lower_throttle;
 } flash_storage_t;
 
 extern flash_storage_t flash_storage;
 
 #define BIND_STORAGE_OFFSET (FLASH_STORAGE_OFFSET + FLASH_STORAGE_SIZE)
-#define BIND_STORAGE_SIZE 64
+#define BIND_STORAGE_SIZE FLASH_ALIGN(64)
 #define BIND_RAW_STORAGE_SIZE 63
 
 typedef struct {
@@ -40,10 +40,10 @@ cbor_result_t cbor_encode_rx_bind_storage_t(cbor_value_t *enc, const rx_bind_sto
 cbor_result_t cbor_decode_rx_bind_storage_t(cbor_value_t *enc, rx_bind_storage_t *s);
 
 #define PROFILE_STORAGE_OFFSET (BIND_STORAGE_OFFSET + BIND_STORAGE_SIZE)
-#define PROFILE_STORAGE_SIZE 2048
+#define PROFILE_STORAGE_SIZE FLASH_ALIGN(2048)
 
 #define VTX_STORAGE_OFFSET (PROFILE_STORAGE_OFFSET + PROFILE_STORAGE_SIZE)
-#define VTX_STORAGE_SIZE 512
+#define VTX_STORAGE_SIZE FLASH_ALIGN(512)
 
 #define FMC_END_OFFSET (VTX_STORAGE_OFFSET + VTX_STORAGE_SIZE)
 
