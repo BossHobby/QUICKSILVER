c5acc8dd9264b73612e299519ce0c9302f0ea90c
chaotixtech <chaotix.f@gmail.com>
2022-05-01 14:59:11 +0100

    geprcf411 target

---
+++ src/targets/geprcf411/target.h
@@ -0,0 +1,71 @@
+
+#include "config.h"
+#include "config_helper.h"
+
+#define GEPRCF411
+
+#define F4
+#define F411
+
+//PORTS
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7  \
+  SPI2_PB13PB14PB15  \
+
+#define USART_PORTS \
+	USART1_PA10PA9 \
+	USART2_PA3PA2
+
+//LEDS
+#define LED_NUMBER 2
+#define LED1PIN PIN_C13
+#define LED1_INVERT
+#define LED2PIN PIN_C14
+#define LED2_INVERT
+
+#define BUZZER_PIN PIN_B2
+
+//GYRO
+#define MPU6XXX_SPI_PORT SPI_PORT1
+#define MPU6XXX_NSS PIN_A4
+#define MPU6XXX_INT PIN_A1
+#define GYRO_ORIENTATION GYRO_ROTATE_90_CCW
+#define USE_DUMMY_I2C
+
+//RADIO
+
+
+#ifdef SERIAL_RX
+#define RX_USART USART_PORT2
+#define SOFTSPI_NONE
+#endif
+
+// OSD
+#define ENABLE_OSD
+#define MAX7456_SPI_PORT SPI_PORT2
+#define MAX7456_NSS PIN_B12
+
+
+
+
+
+//VOLTAGE DIVIDER
+#define BATTERYPIN PIN_B0
+#define BATTERY_ADC_CHANNEL ADC_Channel_8
+#define VOLTAGE_DIVIDER_R1 10000
+#define VOLTAGE_DIVIDER_R2 1000
+
+#define ADC_REF_VOLTAGE 3.3
+
+#define IBAT_PIN PIN_B1
+#define IBAT_SCALE 179
+
+// MOTOR PINS
+//S3_OUT
+#define MOTOR_PIN0 MOTOR_PIN_PB6
+//S4_OUT
+#define MOTOR_PIN1 MOTOR_PIN_PB7
+//S1_OUT
+#define MOTOR_PIN2 MOTOR_PIN_PB4
+//S2_OUT
+#define MOTOR_PIN3 MOTOR_PIN_PB5
