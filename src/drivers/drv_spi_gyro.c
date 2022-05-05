1db75c3ec238d93819ffcba52aa6ca90558b52b7
bkleiner <b.kleiner@str8labs.com>
2022-05-06 00:31:51 +0200

    bmi270: 16bit registers, stupid

--- src/drivers/drv_spi_gyro.c
+++ src/drivers/drv_spi_gyro.c
@@ -117,15 +117,15 @@ gyro_data_t gyro_spi_read() {
     uint8_t buf[14];
     mpu6xxx_read_data(MPU_RA_ACCEL_XOUT_H, buf, 14);
 
-    data.accel.axis[0] = -(int16_t)((buf[0] << 8) + buf[1]);
-    data.accel.axis[1] = -(int16_t)((buf[2] << 8) + buf[3]);
-    data.accel.axis[2] = (int16_t)((buf[4] << 8) + buf[5]);
+    data.accel.axis[0] = -(int16_t)((buf[0] << 8) | buf[1]);
+    data.accel.axis[1] = -(int16_t)((buf[2] << 8) | buf[3]);
+    data.accel.axis[2] = (int16_t)((buf[4] << 8) | buf[5]);
 
-    data.temp = (float)((int16_t)((buf[6] << 8) + buf[7])) / 333.87f + 21.f;
+    data.temp = (float)((int16_t)((buf[6] << 8) | buf[7])) / 333.87f + 21.f;
 
-    data.gyro.axis[1] = (int16_t)((buf[8] << 8) + buf[9]);
-    data.gyro.axis[0] = (int16_t)((buf[10] << 8) + buf[11]);
-    data.gyro.axis[2] = (int16_t)((buf[12] << 8) + buf[13]);
+    data.gyro.axis[1] = (int16_t)((buf[8] << 8) | buf[9]);
+    data.gyro.axis[0] = (int16_t)((buf[10] << 8) | buf[11]);
+    data.gyro.axis[2] = (int16_t)((buf[12] << 8) | buf[13]);
     break;
   }
 
@@ -134,15 +134,15 @@ gyro_data_t gyro_spi_read() {
     uint8_t buf[14];
     icm42605_read_data(ICM42605_TEMP_DATA1, buf, 14);
 
-    data.temp = (float)((int16_t)((buf[0] << 8) + buf[1])) / 132.48f + 25.f;
+    data.temp = (float)((int16_t)((buf[0] << 8) | buf[1])) / 132.48f + 25.f;
 
-    data.accel.axis[0] = -(int16_t)((buf[2] << 8) + buf[3]);
-    data.accel.axis[1] = -(int16_t)((buf[4] << 8) + buf[5]);
-    data.accel.axis[2] = (int16_t)((buf[6] << 8) + buf[7]);
+    data.accel.axis[0] = -(int16_t)((buf[2] << 8) | buf[3]);
+    data.accel.axis[1] = -(int16_t)((buf[4] << 8) | buf[5]);
+    data.accel.axis[2] = (int16_t)((buf[6] << 8) | buf[7]);
 
-    data.gyro.axis[1] = (int16_t)((buf[8] << 8) + buf[9]);
-    data.gyro.axis[0] = (int16_t)((buf[10] << 8) + buf[11]);
-    data.gyro.axis[2] = (int16_t)((buf[12] << 8) + buf[13]);
+    data.gyro.axis[1] = (int16_t)((buf[8] << 8) | buf[9]);
+    data.gyro.axis[0] = (int16_t)((buf[10] << 8) | buf[11]);
+    data.gyro.axis[2] = (int16_t)((buf[12] << 8) | buf[13]);
 
     break;
   }
@@ -151,13 +151,13 @@ gyro_data_t gyro_spi_read() {
     uint8_t buf[12];
     bmi270_read_data(BMI270_REG_ACC_DATA_X_LSB, buf, 12);
 
-    data.accel.axis[0] = -(int16_t)((buf[1] << 8) + buf[0]);
-    data.accel.axis[1] = -(int16_t)((buf[3] << 8) + buf[2]);
-    data.accel.axis[2] = (int16_t)((buf[5] << 8) + buf[4]);
+    data.accel.axis[0] = -(int16_t)((buf[1] << 8) | buf[0]);
+    data.accel.axis[1] = -(int16_t)((buf[3] << 8) | buf[2]);
+    data.accel.axis[2] = (int16_t)((buf[5] << 8) | buf[4]);
 
-    data.gyro.axis[1] = (int16_t)((buf[7] << 8) + buf[6]);
-    data.gyro.axis[0] = (int16_t)((buf[9] << 8) + buf[8]);
-    data.gyro.axis[2] = (int16_t)((buf[11] << 8) + buf[10]);
+    data.gyro.axis[1] = (int16_t)((buf[7] << 8) | buf[6]);
+    data.gyro.axis[0] = (int16_t)((buf[9] << 8) | buf[8]);
+    data.gyro.axis[2] = (int16_t)((buf[11] << 8) | buf[10]);
     break;
   }
 
