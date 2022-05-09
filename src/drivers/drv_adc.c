bc1040938182b098ad38088680b4d659887642b7
bkleiner <b.kleiner@str8labs.com>
2022-05-09 02:58:00 +0200

    adc: add device management

--- src/drivers/drv_adc.c
+++ src/drivers/drv_adc.c
@@ -2,6 +2,7 @@
 
 #include "debug.h"
 #include "drv_gpio.h"
+#include "drv_time.h"
 #include "flight/filter.h"
 #include "profile.h"
 #include "project.h"
@@ -9,52 +10,101 @@
 
 #ifndef DISABLE_ADC
 
-#define ADC_CHANNEL_MAP_SIZE 16
+typedef enum {
+  ADC_DEVICE_1,
+#ifdef STM32H7
+  ADC_DEVICE_2,
+  ADC_DEVICE_3,
+#endif
+  ADC_DEVICE_MAX,
+} adc_devs_t;
+
+typedef struct {
+  ADC_TypeDef *adc;
+  ADC_Common_TypeDef *common;
+} adc_dev_t;
+
+typedef struct {
+  gpio_pins_t pin;
+  adc_devs_t dev;
+  uint32_t channel;
+} adc_channel_t;
 
 #ifndef ADC_REF_VOLTAGE
 #define ADC_REF_VOLTAGE 3300
 #endif
 
 #ifdef STM32H7
-#define ADC ADC12_COMMON
 #define ADC_SAMPLINGTIME LL_ADC_SAMPLINGTIME_387CYCLES_5
+#define READY_TO_CONVERT(dev) LL_ADC_IsActiveFlag_EOC(dev)
 #else
 #define ADC_SAMPLINGTIME LL_ADC_SAMPLINGTIME_480CYCLES
+#define READY_TO_CONVERT(dev) LL_ADC_IsActiveFlag_EOCS(dev)
 #endif
 
 // 12 bit ADC has 4096 steps
 #define ADC_SCALEFACTOR ((float)(ADC_REF_VOLTAGE) / 4096.f)
 #define VBAT_SCALE ((float)(VBAT_DIVIDER_R1 + VBAT_DIVIDER_R2) / (float)(VBAT_DIVIDER_R2) * (1.f / 1000000.f))
 
-typedef struct {
-  gpio_pins_t pin;
-  uint32_t channel;
-} adc_channel_t;
-
-extern profile_t profile;
-
-float vref_cal = 0;
-uint16_t adc_array[ADC_CHAN_MAX];
-adc_channel_t adc_pins[ADC_CHAN_MAX];
-
-static const adc_channel_t adc_channel_map[ADC_CHANNEL_MAP_SIZE] = {
-    {.pin = PIN_C0, .channel = LL_ADC_CHANNEL_10},
-    {.pin = PIN_C1, .channel = LL_ADC_CHANNEL_11},
-    {.pin = PIN_C2, .channel = LL_ADC_CHANNEL_12},
-    {.pin = PIN_C3, .channel = LL_ADC_CHANNEL_13},
-    {.pin = PIN_C4, .channel = LL_ADC_CHANNEL_14},
-    {.pin = PIN_C5, .channel = LL_ADC_CHANNEL_15},
-    {.pin = PIN_B0, .channel = LL_ADC_CHANNEL_8},
-    {.pin = PIN_B1, .channel = LL_ADC_CHANNEL_9},
-    {.pin = PIN_A0, .channel = LL_ADC_CHANNEL_0},
-    {.pin = PIN_A1, .channel = LL_ADC_CHANNEL_1},
-    {.pin = PIN_A2, .channel = LL_ADC_CHANNEL_2},
-    {.pin = PIN_A3, .channel = LL_ADC_CHANNEL_3},
-    {.pin = PIN_A4, .channel = LL_ADC_CHANNEL_4},
-    {.pin = PIN_A5, .channel = LL_ADC_CHANNEL_5},
-    {.pin = PIN_A6, .channel = LL_ADC_CHANNEL_6},
-    {.pin = PIN_A7, .channel = LL_ADC_CHANNEL_7},
+#ifdef STM32H7
+static const adc_dev_t adc_dev[ADC_DEVICE_MAX] = {
+    {.adc = ADC1, .common = ADC12_COMMON},
+    {.adc = ADC2, .common = ADC12_COMMON},
+    {.adc = ADC3, .common = ADC3_COMMON},
 };
+
+static const adc_channel_t adc_channel_map[] = {
+    {.pin = PIN_NONE, .dev = ADC_DEVICE_3, .channel = LL_ADC_CHANNEL_VREFINT},
+
+    {.pin = PIN_C0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_10},
+    {.pin = PIN_C1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_11},
+    {.pin = PIN_C2, .dev = ADC_DEVICE_3, .channel = LL_ADC_CHANNEL_0},
+    {.pin = PIN_C3, .dev = ADC_DEVICE_3, .channel = LL_ADC_CHANNEL_1},
+    {.pin = PIN_C4, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_4},
+    {.pin = PIN_C5, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_8},
+    {.pin = PIN_B0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_9},
+    {.pin = PIN_B1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_5},
+    {.pin = PIN_A0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_16},
+    {.pin = PIN_A1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_17},
+    {.pin = PIN_A2, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_14},
+    {.pin = PIN_A3, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_15},
+    {.pin = PIN_A4, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_18},
+    {.pin = PIN_A5, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_19},
+    {.pin = PIN_A6, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_3},
+    {.pin = PIN_A7, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_7},
+};
+#else
+static const adc_dev_t adc_dev[ADC_DEVICE_MAX] = {
+    {.adc = ADC1, .common = ADC},
+};
+
+static const adc_channel_t adc_channel_map[] = {
+    {.pin = PIN_NONE, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_VREFINT},
+
+    {.pin = PIN_C0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_10},
+    {.pin = PIN_C1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_11},
+    {.pin = PIN_C2, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_12},
+    {.pin = PIN_C3, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_13},
+    {.pin = PIN_C4, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_14},
+    {.pin = PIN_C5, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_15},
+    {.pin = PIN_B0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_8},
+    {.pin = PIN_B1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_9},
+    {.pin = PIN_A0, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_0},
+    {.pin = PIN_A1, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_1},
+    {.pin = PIN_A2, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_2},
+    {.pin = PIN_A3, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_3},
+    {.pin = PIN_A4, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_4},
+    {.pin = PIN_A5, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_5},
+    {.pin = PIN_A6, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_6},
+    {.pin = PIN_A7, .dev = ADC_DEVICE_1, .channel = LL_ADC_CHANNEL_7},
+};
+#endif
+
+#define ADC_CHANNEL_MAP_SIZE (sizeof(adc_channel_map) / sizeof(adc_channel_t))
+
+static float vref_cal = 0;
+static uint16_t adc_array[ADC_CHAN_MAX];
+static adc_channel_t adc_pins[ADC_CHAN_MAX];
 
 static uint16_t adc_calibration_value() {
 #if defined(STM32F4)
@@ -64,17 +114,19 @@ static uint16_t adc_calibration_value() {
 #elif defined(STM32F722)
   return *((uint16_t *)0x1FF07A2A);
 #elif defined(STM32H7)
-  return *VREFINT_CAL_ADDR;
+  return (*VREFINT_CAL_ADDR) >> 4;
 #endif
 }
 
 static void adc_init_pin(adc_chan_t chan, gpio_pins_t pin) {
   adc_array[chan] = 0;
   adc_pins[chan].pin = PIN_NONE;
+  adc_pins[chan].dev = ADC_DEVICE_MAX;
 
   for (uint32_t i = 0; i < ADC_CHANNEL_MAP_SIZE; i++) {
     if (adc_channel_map[i].pin == pin) {
       adc_pins[chan].pin = pin;
+      adc_pins[chan].dev = adc_channel_map[i].dev;
       adc_pins[chan].channel = adc_channel_map[i].channel;
       break;
     }
@@ -90,28 +142,15 @@ static void adc_init_pin(adc_chan_t chan, gpio_pins_t pin) {
   gpio_pin_init(&gpio_init, pin);
 }
 
-void adc_init() {
-  // vref_int only exists on ADC1
-  // example case: pc2 additional function ADC123_IN12
-  // adc1,2,3 connected to APB2 bus
+static void adc_init_dev(adc_devs_t index) {
+  const adc_dev_t *dev = &adc_dev[index];
 
-#ifdef STM32H7
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_ADC12);
-#else
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_ADC1);
-#endif
-
-#ifdef VBAT_PIN
-  adc_init_pin(ADC_CHAN_VBAT, VBAT_PIN);
-#endif
-
-#ifdef IBAT_PIN
-  adc_init_pin(ADC_CHAN_IBAT, IBAT_PIN);
-#endif
-
-  LL_ADC_CommonInitTypeDef adc_common_init;
-  adc_common_init.CommonClock = LL_ADC_CLOCK_SYNC_PCLK_DIV2;
-  LL_ADC_CommonInit(ADC, &adc_common_init);
+  if (!__LL_ADC_IS_ENABLED_ALL_COMMON_INSTANCE(dev->common)) {
+    LL_ADC_CommonInitTypeDef adc_common_init;
+    adc_common_init.CommonClock = LL_ADC_CLOCK_SYNC_PCLK_DIV4;
+    adc_common_init.Multimode = LL_ADC_MULTI_INDEPENDENT;
+    LL_ADC_CommonInit(dev->common, &adc_common_init);
+  }
 
   LL_ADC_REG_InitTypeDef adc_reg_init;
   adc_reg_init.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
@@ -124,7 +163,7 @@ void adc_init() {
 #else
   adc_reg_init.DMATransfer = LL_ADC_REG_DMA_TRANSFER_NONE;
 #endif
-  LL_ADC_REG_Init(ADC1, &adc_reg_init);
+  LL_ADC_REG_Init(dev->adc, &adc_reg_init);
 
   LL_ADC_InitTypeDef adc_init;
   adc_init.Resolution = LL_ADC_RESOLUTION_12B;
@@ -135,19 +174,71 @@ void adc_init() {
   adc_init.DataAlignment = LL_ADC_DATA_ALIGN_RIGHT;
   adc_init.SequencersScanMode = LL_ADC_SEQ_SCAN_DISABLE;
 #endif
-  LL_ADC_Init(ADC1, &adc_init);
+  LL_ADC_Init(dev->adc, &adc_init);
 
-  LL_ADC_SetCommonPathInternalCh(ADC, LL_ADC_PATH_INTERNAL_VREFINT);
-  LL_ADC_Enable(ADC1);
-
-  LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_VREFINT);
-  LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_VREFINT, ADC_SAMPLINGTIME);
+  if (adc_pins[ADC_CHAN_VREF].dev == index) {
+    LL_ADC_SetCommonPathInternalCh(dev->common, LL_ADC_PATH_INTERNAL_VREFINT);
+  }
 
 #ifdef STM32H7
-  LL_ADC_REG_StartConversion(ADC1);
-#else
-  LL_ADC_REG_StartConversionSWStart(ADC1);
+  LL_ADC_DisableDeepPowerDown(dev->adc);
+  LL_ADC_EnableInternalRegulator(dev->adc);
+  time_delay_us(LL_ADC_DELAY_INTERNAL_REGUL_STAB_US);
+
+  while (LL_ADC_IsCalibrationOnGoing(dev->adc) != 0)
+    ;
+
+  // should be cycles, but just use us to be sure
+  time_delay_us(LL_ADC_DELAY_CALIB_ENABLE_ADC_CYCLES);
 #endif
+
+  LL_ADC_Enable(dev->adc);
+
+#ifdef STM32H7
+  while (LL_ADC_IsActiveFlag_ADRDY(dev->adc) == 0)
+    ;
+#endif
+}
+
+static void adc_start_conversion(adc_chan_t index) {
+  const adc_channel_t *chan = &adc_pins[index];
+  const adc_dev_t *dev = &adc_dev[chan->dev];
+
+#ifdef STM32H7
+  LL_ADC_SetChannelPreSelection(dev->adc, chan->channel);
+#endif
+
+  LL_ADC_SetChannelSamplingTime(dev->adc, chan->channel, ADC_SAMPLINGTIME);
+  LL_ADC_REG_SetSequencerRanks(dev->adc, LL_ADC_REG_RANK_1, chan->channel);
+
+#ifdef STM32H7
+  LL_ADC_REG_StartConversion(dev->adc);
+#else
+  LL_ADC_REG_StartConversionSWStart(dev->adc);
+#endif
+}
+
+void adc_init() {
+#ifdef STM32H7
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_ADC12);
+  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_ADC3);
+#else
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_ADC1);
+#endif
+
+  adc_init_pin(ADC_CHAN_VREF, PIN_NONE);
+#ifdef VBAT_PIN
+  adc_init_pin(ADC_CHAN_VBAT, VBAT_PIN);
+#endif
+#ifdef IBAT_PIN
+  adc_init_pin(ADC_CHAN_IBAT, IBAT_PIN);
+#endif
+
+  for (uint32_t i = 0; i < ADC_DEVICE_MAX; i++) {
+    adc_init_dev(i);
+  }
+
+  adc_start_conversion(ADC_CHAN_VREF);
 
   // reference is measured a 3.3v, toy boards are powered by 2.8, so a 1.17 multiplier
   // different vccs will translate to a different adc scale factor,
@@ -155,48 +246,24 @@ void adc_init() {
   vref_cal = 1.0f / ((3.3f / (float)ADC_REF_VOLTAGE) * (float)(adc_calibration_value()));
 }
 
-static uint16_t adc_read_raw(adc_chan_t chan) {
-// will skip if adc is still busy or update the adc_array and request the next conversion
-#ifdef STM32H7
-  uint8_t ready_to_convert = LL_ADC_IsActiveFlag_EOC(ADC1);
-#else
-  uint8_t ready_to_convert = LL_ADC_IsActiveFlag_EOCS(ADC1);
-#endif
-  if (ready_to_convert) {
-    static adc_chan_t last_adc_channel = ADC_CHAN_VREF;
+static uint16_t adc_read_raw(adc_chan_t index) {
+  static adc_chan_t last_adc_chan = ADC_CHAN_VREF;
 
-    // Shove the last adc conversion into the array
-    adc_array[last_adc_channel] = LL_ADC_REG_ReadConversionData12(ADC1);
+  const adc_channel_t *chan = &adc_pins[last_adc_chan];
+  const adc_dev_t *dev = &adc_dev[chan->dev];
 
-    // skip through all channels without a pin
+  if (READY_TO_CONVERT(dev->adc)) {
+    adc_array[last_adc_chan] = LL_ADC_REG_ReadConversionData12(dev->adc);
+
     do {
-      last_adc_channel++;
-      if (last_adc_channel == ADC_CHAN_MAX) {
-        last_adc_channel = 0; // Start the index over if we reached the end of the list
-      }
-    } while (last_adc_channel != ADC_CHAN_VREF && adc_pins[last_adc_channel].pin == PIN_NONE);
+      last_adc_chan = (last_adc_chan + 1) % ADC_CHAN_MAX;
+      // skip through all channels without a dev
+    } while (adc_pins[last_adc_chan].dev == ADC_DEVICE_MAX);
 
-    // Select the new channel to read
-    switch (last_adc_channel) {
-    case ADC_CHAN_VREF:
-      LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_VREFINT, ADC_SAMPLINGTIME);
-      LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_VREFINT);
-      break;
-
-    default: {
-      LL_ADC_SetChannelSamplingTime(ADC1, adc_pins[last_adc_channel].channel, ADC_SAMPLINGTIME);
-      LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, adc_pins[last_adc_channel].channel);
-      break;
-    }
-    }
-
-#ifdef STM32H7
-    LL_ADC_REG_StartConversion(ADC1);
-#else
-    LL_ADC_REG_StartConversionSWStart(ADC1);
-#endif
+    adc_start_conversion(last_adc_chan);
   }
-  return adc_array[chan];
+
+  return adc_array[index];
 }
 
 float adc_read(adc_chan_t chan) {
