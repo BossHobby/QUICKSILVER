9816d798d0b8eba84ed5b2507abb6a8fec27f629
bkleiner <b.kleiner@str8labs.com>
2022-06-11 03:27:21 +0200

    circular_buffer: use atomic block

--- src/util/circular_buffer.c
+++ src/util/circular_buffer.c
@@ -1,66 +1,75 @@
 #include "circular_buffer.h"
 
+#include "drv_interrupt.h"
+
 uint32_t circular_buffer_free(circular_buffer_t *c) {
-  if (c->head >= c->tail) {
-    return (c->size - c->head) + c->tail;
+  ATOMIC_BLOCK(MAX_PRIORITY) {
+    if (c->head >= c->tail) {
+      return (c->size - c->head) + c->tail;
+    }
+    return (c->tail - c->head);
   }
-  return (c->tail - c->head);
+  return 0;
 }
 
 uint8_t circular_buffer_write(circular_buffer_t *c, uint8_t data) {
-  uint32_t next = c->head + 1;
-  if (next >= c->size)
-    next = 0;
+  ATOMIC_BLOCK(MAX_PRIORITY) {
+    const uint32_t next = (c->head + 1) % c->size;
+    if (next == c->tail)
+      return 0;
 
-  if (next == c->tail)
-    return 0;
-
-  c->buffer[c->head] = data;
-  c->head = next;
-  return 1;
+    c->buffer[c->head] = data;
+    c->head = next;
+    return 1;
+  }
+  return 0;
 }
 
 uint32_t circular_buffer_write_multi(circular_buffer_t *c, const uint8_t *data, const uint32_t len) {
   for (uint32_t i = 0; i < len; i++) {
-    uint32_t next = (c->head + 1) % c->size;
+    ATOMIC_BLOCK(MAX_PRIORITY) {
+      const uint32_t next = (c->head + 1) % c->size;
+      if (next == c->tail)
+        return i;
 
-    if (next == c->tail)
-      return i;
-
-    c->buffer[c->head] = data[i];
-    c->head = next;
+      c->buffer[c->head] = data[i];
+      c->head = next;
+    }
   }
   return len;
 }
 
 uint32_t circular_buffer_available(circular_buffer_t *c) {
-  if (c->head >= c->tail) {
-    return c->head - c->tail;
+  ATOMIC_BLOCK(MAX_PRIORITY) {
+    if (c->head >= c->tail) {
+      return c->head - c->tail;
+    }
+    return c->size + c->head - c->tail;
   }
-  return c->size + c->head - c->tail;
+  return 0;
 }
 
 uint8_t circular_buffer_read(circular_buffer_t *c, uint8_t *data) {
-  if (c->head == c->tail)
-    return 0;
+  ATOMIC_BLOCK(MAX_PRIORITY) {
+    if (c->head == c->tail)
+      return 0;
 
-  uint32_t next = c->tail + 1;
-  if (next >= c->size)
-    next = 0;
-
-  *data = c->buffer[c->tail];
-  c->tail = next;
-  return 1;
+    *data = c->buffer[c->tail];
+    c->tail = (c->tail + 1) % c->size;
+    return 1;
+  }
+  return 0;
 }
 
 uint32_t circular_buffer_read_multi(circular_buffer_t *c, uint8_t *data, const uint32_t len) {
   for (uint32_t i = 0; i < len; i++) {
-    if (c->head == c->tail)
-      return i;
+    ATOMIC_BLOCK(MAX_PRIORITY) {
+      if (c->head == c->tail)
+        return i;
 
-    uint32_t next = (c->tail + 1) % c->size;
-    data[i] = c->buffer[c->tail];
-    c->tail = next;
+      data[i] = c->buffer[c->tail];
+      c->tail = (c->tail + 1) % c->size;
+    }
   }
   return len;
 }
