364ae90be8179e9079ed6fdbd943024dc270f7f9
bkleiner <b.kleiner@str8labs.com>
2022-07-09 03:43:46 +0200

    serial_passthrough: ensure all the data is written out

--- src/drivers/drv_serial.c
+++ src/drivers/drv_serial.c
@@ -323,8 +323,12 @@ bool serial_write_bytes(serial_port_t *serial, const uint8_t *data, const uint32
 
   const usart_port_def_t *port = &usart_port_defs[serial->port];
 
-  circular_buffer_write_multi(serial->tx_buffer, data, size);
-  LL_USART_EnableIT_TXE(port->channel);
+  uint32_t written = 0;
+  while (written < size) {
+    written += circular_buffer_write_multi(serial->tx_buffer, data + written, size - written);
+    LL_USART_EnableIT_TXE(port->channel);
+    __WFI();
+  }
 
   return true;
 }
