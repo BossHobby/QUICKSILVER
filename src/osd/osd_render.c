4e23ef1f14af43ffee680cfad486dc4344659056
bkleiner <b.kleiner@str8labs.com>
2022-07-23 16:29:48 +0200

    osd: show stick throttle instead of applied throttle

--- src/osd/osd_render.c
+++ src/osd/osd_render.c
@@ -647,7 +647,7 @@ static void osd_display_regular() {
 
   case OSD_THROTTLE: {
     osd_start(osd_attr(el), el->pos_x, el->pos_y);
-    osd_write_uint(state.throttle * 100.0f, 4);
+    osd_write_uint(state.rx_filtered.throttle * 100.0f, 4);
     osd_write_char(ICON_THROTTLE);
 
     osd_state.element++;
