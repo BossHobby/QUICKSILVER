bca2f7a0963d4409ce39bb647a2c17e528293652
bkleiner <b.kleiner@str8labs.com>
2022-07-24 19:37:23 +0200

    simplify interrupt priorities

--- src/drivers/drv_interrupt.c
+++ src/drivers/drv_interrupt.c
@@ -1,7 +1,7 @@
 #include "drv_interrupt.h"
 
 void interrupt_enable(IRQn_Type irq, uint32_t prio) {
-  NVIC_SetPriorityGrouping(2);
+  NVIC_SetPriorityGrouping(3);
   NVIC_SetPriority(irq, prio);
   NVIC_EnableIRQ(irq);
 }
--- src/drivers/drv_interrupt.h
+++ src/drivers/drv_interrupt.h
@@ -2,19 +2,19 @@
 
 #include "project.h"
 
-#define MAX_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x0, 0x0)
-#define UART_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x0, 0x1)
-#define TIMER_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x0, 0x2)
-#define DMA_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x0)
-#define EXTI_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x1, 0x1)
-#define USB_PRIORITY NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x3, 0x3)
+#define MAX_PRIORITY 0x1
+#define UART_PRIORITY 0x2
+#define TIMER_PRIORITY 0x3
+#define DMA_PRIORITY 0x4
+#define EXTI_PRIORITY 0x5
+#define USB_PRIORITY 0x6
 
 static inline void __basepri_restore(uint8_t *val) {
   __set_BASEPRI(*val);
 }
 
 static inline uint8_t __basepri_set_max(uint8_t prio) {
-  __set_BASEPRI_MAX(prio);
+  __set_BASEPRI_MAX(prio << (8U - __NVIC_PRIO_BITS));
   return 1;
 }
 
@@ -24,6 +24,25 @@ static inline uint8_t __basepri_set_max(uint8_t prio) {
 // __unused__ attribute is used to supress CLang warning
 #define ATOMIC_BLOCK(prio) \
   for (uint8_t __basepri_save __attribute__((__cleanup__(__basepri_restore), __unused__)) = __get_BASEPRI(), __todo = __basepri_set_max(prio); __todo; __todo = 0)
+
+static inline int __int_disable_irq() {
+  int primask;
+  asm volatile("mrs %0, PRIMASK\n"
+               : "=r"(primask));
+  asm volatile("cpsid i\n");
+  return primask & 1;
+}
+
+static inline void __int_restore_irq(int *primask) {
+  if (!(*primask)) {
+    asm volatile("" ::
+                     : "memory");
+    asm volatile("cpsie i\n");
+  }
+}
+
+#define ATOMIC_BLOCK_ALL \
+  for (int __basepri_save __attribute__((__cleanup__(__int_restore_irq))) = __int_disable_irq(), __todo = 1; __todo; __todo = 0)
 
 void interrupt_enable(IRQn_Type irq, uint32_t prio);
 void interrupt_disable(IRQn_Type irq);
