3f418dddcc1b0f3b3d15fba9af09934dbbc9db45
bkleiner <b.kleiner@str8labs.com>
2022-08-05 00:29:22 +0200

    add automatic target config injection into elf

--- script/post_script.py
+++ script/post_script.py
@@ -7,31 +7,16 @@ import serial.tools.list_ports
 
 Import("env", "projenv")
 
+try:
+    import yaml
+    import cbor2
+    from elftools.elf.elffile import ELFFile
+except ImportError:
+    env.Execute("$PYTHONEXE -m pip install pyelftools pyyaml cbor2")
+    import yaml
+    import cbor2
+    from elftools.elf.elffile import ELFFile
 
-def before_upload(source, target, env):
-    for port in serial.tools.list_ports.grep("USB VID:PID=0483:5740"):
-        with serial.Serial(port.device) as ser:
-            ser.write(b'R\r\n')
-            time.sleep(2)
-
-
-env.AddPreAction("upload", before_upload)
-
-env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
-    env.VerboseAction(" ".join([
-        "$OBJCOPY", "-O", "ihex",
-        "\"$BUILD_DIR/${PROGNAME}.elf\"", "\"$BUILD_DIR/${PROGNAME}.hex\""
-    ]), "Building $BUILD_DIR/${PROGNAME}.hex")
-)
-
-env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.elf",
-    env.VerboseAction(" ".join([
-        "$OBJCOPY", "-O", "ihex",
-        "\"$BUILD_DIR/${PROGNAME}.elf\"", "\"$BUILD_DIR/${PIOENV}.hex\""
-    ]), "Building $BUILD_DIR/${PIOENV}.hex")
-)
 
 git_version = "unknown"
 
@@ -53,7 +38,7 @@ else:
 print("GIT_VERSION: " + git_version)
 
 projenv.Append(CPPDEFINES=[
-    ("TARGET",  env["PIOENV"]),
+    ("TARGET",  env["BOARD_MCU"]),
     ("GIT_VERSION", git_version)
 ])
 
@@ -63,3 +48,49 @@ for scope in ("ASFLAGS", "CCFLAGS", "LINKFLAGS"):
     for option in remove_flags:
         while option in env[scope]:
             env[scope].remove(option)
+
+
+def before_upload(source, target, env):
+    for port in serial.tools.list_ports.grep("USB VID:PID=0483:5740"):
+        with serial.Serial(port.device) as ser:
+            ser.write(b'R\r\n')
+            time.sleep(2)
+
+
+env.AddPreAction("upload", before_upload)
+
+
+def process_elf(source, target, env):
+    magic = 0x12AA0001
+    section_name = ".config_flash"
+
+    target_yaml = env.subst("src/target/$PIOENV/target.yaml")
+    with open(target_yaml, 'r') as f:
+        target_config = yaml.load(f, Loader=yaml.Loader)
+
+    elf_filename = env.subst("$BUILD_DIR/${PROGNAME}.elf")
+
+    with open(elf_filename, 'r+b') as f:
+        elf = ELFFile(f)
+        elf_section = elf.get_section_by_name(section_name)
+
+        section_start = elf_section['sh_offset']
+        section_end = section_start + elf_section['sh_size']
+
+        f.seek(section_start)
+        f.write(magic.to_bytes(4, 'little'))
+        cbor2.dump(target_config, f)
+
+        assert(f.tell() <= section_end)
+
+
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    [
+        process_elf,
+        env.VerboseAction(" ".join([
+            "$OBJCOPY", "-O", "ihex",
+            "\"$BUILD_DIR/${PROGNAME}.elf\"", "\"$BUILD_DIR/${PROGNAME}.hex\""
+        ]), "Building $BUILD_DIR/${PROGNAME}.hex")
+    ]
+)
--- script/pre_script.py
+++ script/pre_script.py
@@ -1,36 +1,53 @@
+import os
+
 Import("env")
 
-optimze_flags = [s for s in env.GetProjectOption("system_flags", "").splitlines() if s]
+optimze_flags = [s for s in env.GetProjectOption(
+    "system_flags", "").splitlines() if s]
 
 linker_flags = []
 
 common_flags = [
-  "-Wdouble-promotion",
-  "-Wunsafe-loop-optimizations",
-  "-fsingle-precision-constant",
-  "-fno-exceptions",
-  "-fno-strict-aliasing",
-	"-fstack-usage",
-	"-fno-stack-protector",
-  "-fomit-frame-pointer",
-	"-fno-unwind-tables",
-  "-fno-asynchronous-unwind-tables",
-	"-fno-math-errno",
-  "-fmerge-all-constants",
-  "-funsafe-loop-optimizations"
+    "-Wdouble-promotion",
+    "-Wunsafe-loop-optimizations",
+    "-fsingle-precision-constant",
+    "-fno-exceptions",
+    "-fno-strict-aliasing",
+    "-fstack-usage",
+    "-fno-stack-protector",
+    "-fomit-frame-pointer",
+    "-fno-unwind-tables",
+    "-fno-asynchronous-unwind-tables",
+    "-fno-math-errno",
+    "-fmerge-all-constants",
+    "-funsafe-loop-optimizations"
 ]
 
 if env.GetBuildType() == "release":
-  common_flags.insert(0, "-flto")
-  common_flags.insert(0, "-O3")
-  common_flags.insert(0, "-s")
+    common_flags.insert(0, "-flto")
+    common_flags.insert(0, "-O3")
+    common_flags.insert(0, "-s")
 else:
-  common_flags.insert(0, "-O1")
+    common_flags.insert(0, "-O1")
 
 env.Append(
-  BUILD_FLAGS=["-std=gnu11"],
-  BUILD_UNFLAGS=["-Og", "-Os"],
-  ASFLAGS=optimze_flags + common_flags,
-  CCFLAGS=linker_flags + optimze_flags + common_flags,
-  LINKFLAGS=linker_flags + optimze_flags + common_flags
+    BUILD_FLAGS=["-std=gnu11"],
+    BUILD_UNFLAGS=["-Og", "-Os"],
+    ASFLAGS=optimze_flags + common_flags,
+    CCFLAGS=linker_flags + optimze_flags + common_flags,
+    LINKFLAGS=linker_flags + optimze_flags + common_flags
 )
+
+# enable once https://github.com/platformio/platformio-core/pull/4380 is resolved
+# def rewrite_source(localenv, node):
+#    dst = node.get_path()
+#    dst = dst.replace(localenv["PIOENV"], localenv["BOARD_MCU"])
+#    dst = os.path.splitext(dst)[0]
+#
+#    return localenv.Object(
+#        source=node,
+#        target=dst
+#    )
+#
+#
+# env.AddBuildMiddleware(rewrite_source)
