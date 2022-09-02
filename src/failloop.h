b3acf15d31e2329f459e28d0fec0ba5f2544aaf5
bkleiner <b.kleiner@str8labs.com>
2022-09-02 23:01:03 +0200

    add DMA failloop

--- src/failloop.h
+++ src/failloop.h
@@ -5,13 +5,13 @@
 
 typedef enum {
   FAILLOOP_NONE = 0,
-  FAILLOOP_LOW_BATTERY = 2, // - low battery at powerup - currently unused
-  FAILLOOP_RADIO = 3,       // - radio chip not found
-  FAILLOOP_GYRO = 4,        // - gyro not found
-  FAILLOOP_FAULT = 5,       // - clock, intterrupts, systick, gcc bad code, bad memory access (code issues like bad pointers) - this should not come up
-  FAILLOOP_LOOPTIME = 6,    // - loop time issue - if loop time exceeds 20mS
-  FAILLOOP_SPI = 7,         // - spi error  - triggered by hardware spi driver only
-  FAILLOOP_SPI_MAIN = 8,    // - spi error main loop  - triggered by hardware spi driver only
+  FAILLOOP_LOW_BATTERY = 2, // low battery at powerup - currently unused
+  FAILLOOP_RADIO = 3,       // radio chip not found
+  FAILLOOP_GYRO = 4,        // gyro not found
+  FAILLOOP_FAULT = 5,       // clock, intterrupts, systick, gcc bad code, bad memory access (code issues like bad pointers) - this should not come up
+  FAILLOOP_LOOPTIME = 6,    // loop time issue - if loop time exceeds 20mS
+  FAILLOOP_DMA = 7,         // dma error - triggered by dma pool
+  FAILLOOP_SPI = 8,         // spi error - triggered by hardware spi driver only
 } failloop_t;
 
 void failloop(failloop_t val);
