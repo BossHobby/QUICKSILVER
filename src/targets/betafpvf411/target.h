747fc2263cd6a50aba4ca3161cd9bccd2797af46
bkleiner <b.kleiner@str8labs.com>
2022-11-23 00:57:23 +0100

    betafpvf411: tidy

--- src/targets/betafpvf411/target.h
+++ src/targets/betafpvf411/target.h
@@ -1,10 +1,9 @@
 #include "config.h"
 
 // PORTS
-#define SPI_PORTS   \
-  SPI1_PA5PA6PA7    \
-  SPI2_PB13PB14PB15 \
-  SPI3_PB3PB4PB5
+#define SPI_PORTS \
+  SPI1_PA5PA6PA7  \
+  SPI2_PB13PB14PB15
 
 #define USART_PORTS \
   USART1_PA10PA9    \
@@ -13,18 +12,12 @@
 
 #define USE_SOFT_SERIAL
 
-//#define USB_DETECT_PIN GPIO_Pin_15
-//#define USB_DETECT_PORT GPIOC
-
 // LEDS
 #define LED_NUMBER 2
 #define LED1PIN PIN_C13
 #define LED2PIN PIN_C14
 
 #define BUZZER_PIN PIN_B2
-
-//#define FPV_PIN GPIO_Pin_13
-//#define FPV_PORT GPIOA
 
 // GYRO
 #define GYRO_SPI_PORT SPI_PORT1
@@ -47,9 +40,14 @@
 #define VBAT_DIVIDER_R2 1000
 
 #define IBAT_PIN PIN_B1
+#define IBAT_SCALE 400
 
 // MOTOR PINS
+// S3_OUT
 #define MOTOR_PIN0 MOTOR_PIN_PB6
+// S4_OUT
 #define MOTOR_PIN1 MOTOR_PIN_PB7
+// S1_OUT
 #define MOTOR_PIN2 MOTOR_PIN_PB4
+// S2_OUT
 #define MOTOR_PIN3 MOTOR_PIN_PB5
