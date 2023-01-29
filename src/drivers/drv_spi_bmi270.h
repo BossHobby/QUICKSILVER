cd6025ce1c57a7f04769b2b596fcc9f40bb22b7a
bkleiner <b.kleiner@str8labs.com>
2023-01-29 19:25:18 +0100

    bmi270: add CAS and IOC

--- src/drivers/drv_spi_bmi270.h
+++ src/drivers/drv_spi_bmi270.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include "drv_spi_gyro.h"
+
 #define BMI270_REG_CHIP_ID 0x00
 #define BMI270_REG_ERR_REG 0x02
 #define BMI270_REG_STATUS 0x03
@@ -18,6 +20,7 @@
 #define BMI270_REG_FIFO_LENGTH_LSB 0x24
 #define BMI270_REG_FIFO_LENGTH_MSB 0x25
 #define BMI270_REG_FIFO_DATA 0x26
+#define BMI270_REG_FEAT_PAGE 0x2F
 #define BMI270_REG_ACC_CONF 0x40
 #define BMI270_REG_ACC_RANGE 0x41
 #define BMI270_REG_GYRO_CONF 0x42
@@ -39,26 +42,30 @@
 #define BMI270_REG_INIT_DATA 0x5E
 #define BMI270_REG_ACC_SELF_TEST 0x6D
 #define BMI270_REG_GYR_SELF_TEST_AXES 0x6E
+#define BMI270_REG_OFFSET_6 0x77
 #define BMI270_REG_PWR_CONF 0x7C
 #define BMI270_REG_PWR_CTRL 0x7D
 #define BMI270_REG_CMD 0x7E
 
+#define BMI270_REG_FEATURES_0_GYR_CAS 0x3C
+#define BMI270_REG_FEATURES_1_GEN_SET_1 0x34
+
 #define BMI270_CMD_SOFTRESET 0xB6
 #define BMI270_CMD_FIFOFLUSH 0xB0
-#define BMI270_PWR_CTRL 0x0E              // enable gyro, acc and temp sensors
-#define BMI270_PWR_CONF 0x02              // disable advanced power save, enable FIFO self-wake
-#define BMI270_ACC_CONF_ODR800 0x0B       // set acc sample rate to 800hz
-#define BMI270_ACC_CONF_ODR1600 0x0C      // set acc sample rate to 1600hz
-#define BMI270_ACC_CONF_BWP 0x02          // set acc filter in normal mode
-#define BMI270_ACC_CONF_HP 0x01           // set acc in high performance mode
-#define BMI270_ACC_RANGE_8G 0x02          // set acc to 8G full scale
-#define BMI270_ACC_RANGE_16G 0x03         // set acc to 16G full scale
-#define BMI270_GYRO_CONF_ODR3200 0x0D     // set gyro sample rate to 3200hz
-#define BMI270_GYRO_CONF_ODR6400 0x0e     // set gyro sample rate to 6400hz
-#define BMI270_GYRO_CONF_ODR12800 0x0f    // set gyro sample rate to 12800hz
-#define BMI270_GYRO_CONF_BWP 0x00         // set gyro filter in osr4 mode
-#define BMI270_GYRO_CONF_NOISE_PERF 0x01  // set gyro in high performance noise mode
-#define BMI270_GYRO_CONF_FILTER_PERF 0x01 // set gyro in high performance filter mode
+#define BMI270_PWR_CTRL 0x0E                    // enable gyro, acc and temp sensors
+#define BMI270_PWR_CONF 0x02                    // disable advanced power save, enable FIFO self-wake
+#define BMI270_ACC_CONF_ODR800 0x0B             // set acc sample rate to 800hz
+#define BMI270_ACC_CONF_ODR1600 0x0C            // set acc sample rate to 1600hz
+#define BMI270_ACC_CONF_BWP 0x02                // set acc filter in normal mode
+#define BMI270_ACC_CONF_HP 0x01                 // set acc in high performance mode
+#define BMI270_ACC_RANGE_8G 0x02                // set acc to 8G full scale
+#define BMI270_ACC_RANGE_16G 0x03               // set acc to 16G full scale
+#define BMI270_GYRO_CONF_ODR3200 0x0D           // set gyro sample rate to 3200hz
+#define BMI270_GYRO_CONF_BWP 0x00               // set gyro filter in osr4 mode
+#define BMI270_GYRO_CONF_NOISE_PERF 0x01        // set gyro in high performance noise mode
+#define BMI270_GYRO_CONF_FILTER_PERF 0x01       // set gyro in high performance filter mode
+#define BMI270_GYRO_CONF_OFFSET_6 0x40          // enable offset compensation
+#define BMI270_CONF_FEATURES_1_GEN_SET_1 0x0288 // enable gyr_self_off
 
 // set gyro to 2000dps full scale
 // for some reason you have to enable the ois_range bit (bit 3) for 2000dps as well
@@ -74,11 +81,16 @@
 #define BMI270_FIFO_WTM_0 0x06             // set the FIFO watermark level to 1 gyro sample (6 bytes)
 #define BMI270_FIFO_WTM_1 0x00             // FIFO watermark MSB
 
+#define BMI270_GYRO_CAS_MASK 0x7F
+#define BMI270_GYRO_CAS_SIGN_BIT_MASK 0x40
+
 uint8_t bmi270_detect();
 void bmi270_configure();
 
 void bmi270_write(uint8_t reg, uint8_t data);
+void bmi270_write16(uint8_t reg, uint16_t data);
 void bmi270_write_data(uint8_t reg, uint8_t *data, uint32_t size);
 
 uint8_t bmi270_read(uint8_t reg);
 void bmi270_read_data(uint8_t reg, uint8_t *data, uint32_t size);
+void bmi270_read_gyro_data(gyro_data_t *data);
