4dfec80a0f4d62420049b3ad24853072e086c5cc
bkleiner <b.kleiner@str8labs.com>
2023-03-21 01:13:31 +0100

    target: add gpio_pin parser

--- src/core/target.c
+++ src/core/target.c
@@ -1,5 +1,9 @@
 #include "core/target.h"
 
+#include <string.h>
+
+#include "driver/gpio.h"
+#include "io/quic.h"
 #include "util/cbor_helper.h"
 
 target_t target = {
@@ -37,3 +41,112 @@ CBOR_END_STRUCT_DECODER()
 #undef TSTR_MEMBER
 #undef ARRAY_MEMBER
 #undef STR_ARRAY_MEMBER
+
+static const uint8_t pin_none_str[] = "NONE";
+
+typedef enum {
+  GPIO_PORTA,
+  GPIO_PORTB,
+  GPIO_PORTC,
+  GPIO_PORTD,
+  GPIO_PORTE,
+  GPIO_PORTF,
+  GPIO_PORTG,
+  GPIO_PORTH,
+  GPIO_PORTI,
+  GPIO_PORTJ,
+  GPIO_PORTK,
+  GPIO_PORTL,
+  GPIO_PORTM,
+  GPIO_PORTN,
+  GPIO_PORTO,
+  GPIO_PORTP,
+  GPIO_PORT_MAX,
+} gpio_ports_t;
+
+#define GPIO_PIN_MAX 16
+
+#define GPIO_AF(pin, af, tag)
+#define GPIO_PIN(_port, _num) \
+  [GPIO_PORT##_port * GPIO_PIN_MAX + _num] = PIN_##_port##_num,
+
+static const gpio_pins_t gpio_pin_lookup[GPIO_PORT_MAX * GPIO_PIN_MAX] = {
+#include "hardware/gpio_pins.in"
+};
+
+#undef GPIO_PIN
+#undef GPIO_AF
+
+#define GPIO_AF(pin, af, tag)
+#define GPIO_PIN(_port, _num) \
+  [PIN_##_port##_num] = GPIO_PORT##_port,
+
+static const gpio_ports_t gpio_port_lookup[PINS_MAX] = {
+#include "hardware/gpio_pins.in"
+};
+
+#undef GPIO_PIN
+#undef GPIO_AF
+
+cbor_result_t cbor_encode_gpio_pins_t(cbor_value_t *enc, const gpio_pins_t *t) {
+  cbor_result_t res = CBOR_OK;
+
+  uint8_t buf[4];
+  uint8_t size = 3;
+
+  if (*t != PIN_NONE) {
+    const uint8_t port = gpio_port_lookup[*t];
+    const uint8_t pin = gpio_pin_defs[*t].pin_index;
+
+    buf[0] = 'P';
+    buf[1] = 'A' + port;
+
+    if (pin >= 10) {
+      buf[2] = '1';
+      buf[3] = '0' + pin % 10;
+      size = 4;
+    } else {
+      buf[2] = '0' + pin;
+    }
+  } else {
+    memcpy(buf, pin_none_str, 4);
+    size = 4;
+  }
+
+  CBOR_CHECK_ERROR(res = cbor_encode_tstr(enc, buf, size));
+
+  return res;
+}
+
+cbor_result_t cbor_decode_gpio_pins_t(cbor_value_t *dec, gpio_pins_t *t) {
+  cbor_result_t res = CBOR_OK;
+
+  const uint8_t *buf;
+  uint32_t size;
+  CBOR_CHECK_ERROR(res = cbor_decode_tstr(dec, &buf, &size));
+
+  if (size > 4 && size < 3) {
+    return CBOR_ERR_INVALID_TYPE;
+  }
+
+  if (size == 4 && memcmp(buf, pin_none_str, 4) == 0) {
+    *t = PIN_NONE;
+    return res;
+  }
+
+  if (buf[0] != 'P') {
+    return CBOR_ERR_INVALID_TYPE;
+  }
+
+  uint32_t val = (buf[1] - 'A') * GPIO_PORT_MAX;
+  if (size == 4) {
+    val += 10;
+    val += buf[3] - '0';
+  } else {
+    val += buf[2] - '0';
+  }
+
+  *t = gpio_pin_lookup[val];
+
+  return res;
+}
--- src/core/target.h
+++ src/core/target.h
@@ -2,6 +2,8 @@
 
 #include <cbor.h>
 
+#include "gpio_pins.h"
+
 typedef struct {
   uint8_t name[32];
 } target_t;
@@ -10,6 +12,9 @@ typedef struct {
   TSTR_MEMBER(name, 32)
 
 extern target_t target;
+
+cbor_result_t cbor_encode_gpio_pins_t(cbor_value_t *enc, const gpio_pins_t *t);
+cbor_result_t cbor_decode_gpio_pins_t(cbor_value_t *dec, gpio_pins_t *t);
 
 cbor_result_t cbor_encode_target_t(cbor_value_t *enc, const target_t *t);
 cbor_result_t cbor_decode_target_t(cbor_value_t *dec, target_t *t);
