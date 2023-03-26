89aabf47edb0e169a393374701cfde79a4d78f40
bkleiner <b.kleiner@str8labs.com>
2023-03-26 19:15:31 +0200

    blackbox_fieldflags => field_flags they never occur out of context

--- src/io/blackbox_device.h
+++ src/io/blackbox_device.h
@@ -19,18 +19,18 @@ typedef struct {
 } blackbox_device_bounds_t;
 
 typedef struct {
-  uint32_t blackbox_fieldflags;
+  uint32_t field_flags;
   uint32_t looptime;
   uint8_t blackbox_rate;
   uint32_t start;
   uint32_t size;
 } blackbox_device_file_t;
 
-#define BLACKBOX_DEVICE_FILE_MEMBERS  \
-  MEMBER(blackbox_fieldflags, uint32) \
-  MEMBER(looptime, uint32)            \
-  MEMBER(blackbox_rate, uint8)        \
-  MEMBER(start, uint32)               \
+#define BLACKBOX_DEVICE_FILE_MEMBERS \
+  MEMBER(field_flags, uint32)        \
+  MEMBER(looptime, uint32)           \
+  MEMBER(blackbox_rate, uint8)       \
+  MEMBER(start, uint32)              \
   MEMBER(size, uint32)
 
 #define BLACKBOX_DEVICE_MAX_FILES 10
@@ -71,8 +71,8 @@ uint32_t blackbox_device_usage();
 blackbox_device_file_t *blackbox_current_file();
 
 void blackbox_device_reset();
-bool blackbox_device_restart(uint32_t blackbox_fieldflags, uint32_t blackbox_rate, uint32_t looptime);
+bool blackbox_device_restart(uint32_t field_flags, uint32_t blackbox_rate, uint32_t looptime);
 void blackbox_device_finish();
 
 void blackbox_device_read_backbox(const uint32_t file_index, const uint32_t offset, uint8_t *buffer, const uint32_t size);
-cbor_result_t blackbox_device_write_backbox(const uint32_t blackbox_fieldflags, const blackbox_t *b);
+cbor_result_t blackbox_device_write_backbox(const uint32_t field_flags, const blackbox_t *b);
