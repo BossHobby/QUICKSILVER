f3dbb2d2ac752204e6f09b115a4bfa877967b63b
bkleiner <b.kleiner@str8labs.com>
2023-06-08 00:46:43 +0200

    at32: add reset code

--- src/driver/reset.h
+++ src/driver/reset.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define RESET_FIRMWARE_MAGIC 0xCAFEBABE
+#define RESET_BOOTLOADER_MAGIC 0xDEADBEEF
+
 void system_reset();
 void system_reset_to_bootloader();
-void system_check_for_bootloader();
