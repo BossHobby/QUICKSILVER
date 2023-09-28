69090f5ccdf8407bbb21309bdd74ed181f2f4708
bkleiner <b.kleiner@str8labs.com>
2023-09-28 16:16:41 +0200

    refactor input_stick_vector call

--- src/flight/input.h
+++ src/flight/input.h
@@ -2,6 +2,6 @@
 
 #include "util/vector.h"
 
-void input_stick_vector(float rx_input[], float maxangle);
+vec3_t input_stick_vector(float rx_input[]);
 vec3_t input_rates_calc();
 float input_throttle_calc(float throttle);
