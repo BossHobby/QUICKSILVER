a926eecc46c5e540140f1af42f07090dd6eb36ed
bkleiner <b.kleiner@str8labs.com>
2023-10-29 02:58:57 +0200

    remove perf counter

--- src/core/debug.h
+++ src/core/debug.h
@@ -6,35 +6,6 @@
 
 #include "io/usb_configurator.h"
 
-typedef enum {
-  PERF_COUNTER_TOTAL,
-  PERF_COUNTER_GYRO,
-  PERF_COUNTER_CONTROL,
-  PERF_COUNTER_RX,
-  PERF_COUNTER_OSD,
-  PERF_COUNTER_MISC,
-  PERF_COUNTER_BLACKBOX,
-  PERF_COUNTER_DEBUG,
-
-  PERF_COUNTER_MAX
-} __attribute__((__packed__)) perf_counters_t;
-
-typedef struct {
-  uint32_t min;
-  uint32_t max;
-  uint32_t current;
-} perf_counter_t;
-
-extern perf_counter_t perf_counters[PERF_COUNTER_MAX];
-
-void perf_counter_start(perf_counters_t counter);
-void perf_counter_end(perf_counters_t counter);
-
-void perf_counter_init();
-void perf_counter_update();
-
-cbor_result_t cbor_encode_perf_counters(cbor_value_t *enc);
-
 void debug_pin_init();
 
 void debug_pin_enable(uint8_t index);
