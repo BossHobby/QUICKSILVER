e99e6dbd8893c7525a7c040c6e8588fa41de33fb
bkleiner <b.kleiner@str8labs.com>
2023-12-21 15:20:34 +0100

    input_stick_vector: use sqrtf

--- src/util/util.c
+++ src/util/util.c
@@ -99,25 +99,6 @@ float atan2approx(float y, float x) {
   return RADTODEG * dphi;
 }
 
-// from http://en.wikipedia.org/wiki/Fast_inverse_square_root
-// originally from quake3 code
-float Q_rsqrt(float number) {
-  long i;
-  float x2, y;
-  const float threehalfs = 1.5F;
-
-  x2 = number * 0.5F;
-  y = number;
-  i = *(long *)&y;
-  i = 0x5f3759df - (i >> 1);
-  y = *(float *)&i;
-  y = y * (threehalfs - (x2 * y * y)); // 1st iteration
-  y = y * (threehalfs - (x2 * y * y)); // 2nd iteration, this can be removed
-                                       //	y  = y * ( threehalfs - ( x2 * y * y ) );   // 3nd iteration, this can be removed
-
-  return y;
-}
-
 int8_t buf_equal(const uint8_t *str1, size_t len1, const uint8_t *str2, size_t len2) {
   if (len2 != len1) {
     return 0;
