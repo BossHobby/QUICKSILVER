da01906ac98c09c9581dfa993749c1cad9f584cf
bkleiner <b.kleiner@str8labs.com>
2024-05-02 00:45:00 +0200

    update libusb

--- lib/libusb_stm32/src/usbd_stm32l433_devfs.c
+++ lib/libusb_stm32/src/usbd_stm32l433_devfs.c
@@ -94,7 +94,7 @@ static uint16_t get_next_pma(uint16_t sz) {
         if ((tbl->rx.addr) && (tbl->rx.addr < _result)) _result = tbl->rx.addr;
         if ((tbl->tx.addr) && (tbl->tx.addr < _result)) _result = tbl->tx.addr;
     }
-    return (_result < (0x020 + sz)) ? 0 : (_result - sz);
+    return (_result < (0x020U + sz)) ? 0 : (_result - sz);
 }
 
 static uint32_t getinfo(void) {
@@ -272,22 +272,20 @@ static void ep_deconfig(uint8_t ep) {
 }
 
 static uint16_t pma_read (uint8_t *buf, uint16_t blen, pma_rec *rx) {
-    uint16_t *pma = (void*)(USB_PMAADDR + rx->addr);
+    uint16_t tmp = 0;
+    volatile uint16_t *pma = (volatile uint16_t *)(USB_PMAADDR + rx->addr);
     uint16_t rxcnt = rx->cnt & 0x03FF;
     rx->cnt &= ~0x3FF;
-
-    if (blen > rxcnt) {
-        blen = rxcnt;
-    }
-    rxcnt = blen;
-    while (blen) {
-        uint16_t _t = *pma;
-        *buf++ = _t & 0xFF;
-        if (--blen) {
-            *buf++ = _t >> 8;
-            pma++;
-            blen--;
-        } else break;
+    for(int idx = 0; idx < rxcnt; idx++) {
+        if ((idx & 0x01) == 0) {
+            tmp = *pma++;
+        }
+        if (idx < blen) {
+            buf[idx] = tmp & 0xFF;
+            tmp >>= 8;
+        } else {
+            return blen;
+        }
     }
     return rxcnt;
 }
@@ -336,14 +334,16 @@ static int32_t ep_read(uint8_t ep, void *buf, uint16_t blen) {
 }
 
 static void pma_write(const uint8_t *buf, uint16_t blen, pma_rec *tx) {
-    uint16_t *pma = (void*)(USB_PMAADDR + tx->addr);
+    volatile uint16_t *pma = (volatile uint16_t *)(USB_PMAADDR + tx->addr);
+    uint16_t tmp = 0;
     tx->cnt = blen;
-    while (blen > 1) {
-        *pma++ = buf[1] << 8 | buf[0];
-        buf += 2;
-        blen -= 2;
+    for (int idx=0; idx < blen; idx++) {
+        tmp |= buf[idx] << ((idx & 0x01) ? 8 : 0);
+        if ((idx & 0x01) || (idx + 1) == blen) {
+            *pma++ = tmp;
+            tmp = 0;
+        }
     }
-    if (blen) *pma = *buf;
 }
 
 static int32_t ep_write(uint8_t ep, const void *buf, uint16_t blen) {
