71f673635fd34b9455c2274fe002c19c30aa7605
bkleiner <benedikt.kleiner@gmail.com>
2024-10-06 18:43:12 +0200

    sixaxis: speed up calibration

--- src/io/led.h
+++ src/io/led.h
@@ -7,7 +7,7 @@
 void led_init();
 void led_on(uint8_t val);
 void led_off(uint8_t val);
-void led_pwm(uint8_t pwmval, float looptime);
+void led_pwm(float brightness, float looptime);
 
 void led_flash();
 void led_blink(uint8_t count);
