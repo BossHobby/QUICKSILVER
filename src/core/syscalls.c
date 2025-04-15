88270fe5695f2339c9145ba8154982f16d3f522a
bkleiner <benedikt.kleiner@gmail.com>
2025-04-15 15:42:02 +0200

    split heap and stack

--- src/core/syscalls.c
+++ src/core/syscalls.c
@@ -2,24 +2,18 @@
 
 #ifndef SIMULATOR
 
-register char *stack_ptr asm("sp");
-uint __heap_limit = 0xcafedead;
+extern uint8_t _heap_start;
+extern uint8_t _heap_end;
 
 __attribute__((__used__)) void *_sbrk(ptrdiff_t incr) {
-  extern char end asm("end"); /* Defined by the linker.  */
-  static char *heap_end;
+  static uint8_t *heap_end = &_heap_end;
 
-  if (heap_end == NULL)
-    heap_end = &end;
-
-  const char *prev_heap_end = heap_end;
-
-  if ((heap_end + incr > stack_ptr) ||
-      (__heap_limit != 0xcafedead && heap_end + incr > (char *)__heap_limit)) {
+  const uint8_t *prev_heap_end = heap_end;
+  if ((heap_end + incr > &_heap_start)) {
     return (void *)-1;
   }
-
   heap_end += incr;
+
   return (void *)prev_heap_end;
 }
 
