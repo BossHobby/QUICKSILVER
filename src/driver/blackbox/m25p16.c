797106b5aed0fa35f290e8f45cedd9b8272d9333
Benedikt Kleiner <benedikt.kleiner@tbs-avionics.com>
2025-12-02 11:36:51 +0800

    add support for more flash chips

--- src/driver/blackbox/m25p16.c
+++ src/driver/blackbox/m25p16.c
@@ -14,13 +14,19 @@
 #define JEDEC_ID_MICRON_M25P16 0x202015
 #define JEDEC_ID_MICRON_N25Q064 0x20BA17
 #define JEDEC_ID_MICRON_N25Q128 0x20ba18
+#define JEDEC_ID_WINBOND_W25Q80 0xEF4014
 #define JEDEC_ID_WINBOND_W25Q16 0xEF4015
+#define JEDEC_ID_WINBOND_W25X32 0xEF3016
 #define JEDEC_ID_WINBOND_W25Q32 0xEF4016
 #define JEDEC_ID_WINBOND_W25Q64 0xEF4017
+#define JEDEC_ID_WINBOND_W25Q64_DTR 0xEF7017
 #define JEDEC_ID_WINBOND_W25Q128 0xEF4018
 #define JEDEC_ID_WINBOND_W25Q128_DTR 0xEF7018
 #define JEDEC_ID_WINBOND_W25Q256 0xEF4019
+#define JEDEC_ID_CYPRESS_S25FL064L 0x016017
 #define JEDEC_ID_CYPRESS_S25FL128L 0x016018
+#define JEDEC_ID_PUYA_PY25Q128 0x852018
+#define JEDEC_ID_ZBIT_ZB25VQ128 0x5E4018
 #define JEDEC_ID_BERGMICRO_W25Q32 0xE04016
 
 #define m25p16_addr(addr) ((addr >> 16) & 0xFF), ((addr >> 8) & 0xFF), (addr & 0xFF)
@@ -182,6 +188,10 @@ void m25p16_get_bounds(blackbox_device_bounds_t *blackbox_bounds) {
 
   const uint32_t chip_id = (raw_id[0] << 16) | (raw_id[1] << 8) | raw_id[2];
   switch (chip_id) {
+  case JEDEC_ID_WINBOND_W25Q80:
+    blackbox_bounds->sectors = 16;
+    blackbox_bounds->pages_per_sector = 256;
+    break;
   case JEDEC_ID_WINBOND_W25Q16:
   case JEDEC_ID_MICRON_M25P16:
     blackbox_bounds->sectors = 32;
@@ -191,6 +201,7 @@ void m25p16_get_bounds(blackbox_device_bounds_t *blackbox_bounds) {
     blackbox_bounds->sectors = 1024;
     blackbox_bounds->pages_per_sector = 16;
     break;
+  case JEDEC_ID_WINBOND_W25X32:
   case JEDEC_ID_WINBOND_W25Q32:
   case JEDEC_ID_MACRONIX_MX25L3206E:
     blackbox_bounds->sectors = 64;
@@ -198,7 +209,9 @@ void m25p16_get_bounds(blackbox_device_bounds_t *blackbox_bounds) {
     break;
   case JEDEC_ID_MICRON_N25Q064:
   case JEDEC_ID_WINBOND_W25Q64:
+  case JEDEC_ID_WINBOND_W25Q64_DTR:
   case JEDEC_ID_MACRONIX_MX25L6406E:
+  case JEDEC_ID_CYPRESS_S25FL064L:
     blackbox_bounds->sectors = 128;
     blackbox_bounds->pages_per_sector = 256;
     break;
@@ -206,6 +219,8 @@ void m25p16_get_bounds(blackbox_device_bounds_t *blackbox_bounds) {
   case JEDEC_ID_WINBOND_W25Q128:
   case JEDEC_ID_WINBOND_W25Q128_DTR:
   case JEDEC_ID_CYPRESS_S25FL128L:
+  case JEDEC_ID_PUYA_PY25Q128:
+  case JEDEC_ID_ZBIT_ZB25VQ128:
     blackbox_bounds->sectors = 256;
     blackbox_bounds->pages_per_sector = 256;
     break;
