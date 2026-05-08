962d93698bd79e23f5687e4bc0e1ebad5d299eb4
Benedikt Kleiner <benedikt.kleiner@tbs-avionics.com>
2026-05-08 11:54:26 +0800

    control: add rover mode

--- src/control/multi/motor.h
+++ src/control/multi/motor.h
@@ -4,7 +4,9 @@
 
 #include "core/project.h"
 
-void motor_test_calc(bool motortest_usb, float mix[MOTOR_PIN_MAX]);
-void motor_mixer_calc(float mix[MOTOR_PIN_MAX]);
+#define MULTI_MOTOR_COUNT 4
 
-void motor_output_calc(float mix[MOTOR_PIN_MAX]);
+void motor_test_calc(bool motortest_usb, float mix[MULTI_MOTOR_COUNT]);
+void motor_mixer_calc(float mix[MULTI_MOTOR_COUNT]);
+
+void motor_output_calc(float mix[MULTI_MOTOR_COUNT]);
---
+++ src/control/rover/control.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdbool.h>
+
+void pid_init();
